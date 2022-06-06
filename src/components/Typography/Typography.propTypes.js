0049fdc7006d6a2bd6072824a7e7efb330ccd14e
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-06-06 11:45:35 +0100

    Change prop types

--- src/components/Typography/Typography.propTypes.js
+++ src/components/Typography/Typography.propTypes.js
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
-import { addCommonPropTypes } from '../../configs/propTypes';
 
-export const PROP_TYPES_TYPOGRAPHY = addCommonPropTypes({
+export const PROP_TYPES_TYPOGRAPHY = {
     children: PropTypes.node,
     variant: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'body', 'caption', 'tiny']),
     color: PropTypes.string,
     as: PropTypes.string,
     ...PROP_TYPES_LAYOUTBOX,
-});
+};
