0e6d4144aee357550d9eabd4f41e25269940c78d
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-07-01 12:12:36 +0100

    Tests

--- src/components/Modal/Modal.test.js
+++ src/components/Modal/Modal.test.js
@@ -1,97 +1,70 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
-import Alert from './Alert';
+import Modal from './Modal';
+import ModalHeader from './ModalHeader';
+import ModalBody from './ModalBody';
+import ModalFooter from './ModalFooter';
 
 const props = {
+    open: true,
     onClose: jest.fn(),
-    variant: 'success',
 };
 
 let wrapper;
 
-describe('Given the Alert component', () => {
+describe('Given the Modal component', () => {
     describe('When it is rendered', () => {
-        beforeEach(() => {
-            wrapper = render(<Alert {...props}>Content</Alert>, {
-                wrapper: Providers,
-            });
+        beforeAll(() => {
+            wrapper = render(
+                <Modal {...props}>
+                    <ModalHeader>Header</ModalHeader>
+                    <ModalBody>Body</ModalBody>
+                    <ModalFooter>Footer</ModalFooter>
+                </Modal>,
+                {
+                    wrapper: Providers,
+                }
+            );
         });
 
         it('Then matches the previous snapshot', async () => {
             expect(wrapper.container).toMatchSnapshot();
         });
 
-        describe('And it is dismissable', () => {
-            beforeEach(() => {
+        it('Then contains the correct content', async () => {
+            expect(wrapper.getByText('Header')).toBeTruthy();
+            expect(wrapper.getByText('Body')).toBeTruthy();
+            expect(wrapper.getByText('Footer')).toBeTruthy();
+        });
+
+        describe('When it is closed', () => {
+            beforeAll(() => {
+                const button = wrapper.container.querySelector('button');
+
+                fireEvent.click(button);
+            });
+
+            it('Then contains the correct content', async () => {
+                expect(wrapper.container.innerHTML).toEqual('');
+            });
+        });
+
+        describe('When it is not open', () => {
+            beforeAll(() => {
                 wrapper = render(
-                    <Alert {...props} dismissable>
-                        Content
-                    </Alert>,
+                    <Modal open={false}>
+                        <ModalHeader>Header</ModalHeader>
+                        <ModalBody>Body</ModalBody>
+                        <ModalFooter>Footer</ModalFooter>
+                    </Modal>,
                     {
                         wrapper: Providers,
                     }
                 );
             });
 
-            afterEach(() => {
-                jest.clearAllMocks();
-            });
-
-            describe('And it is closed', () => {
-                beforeEach(() => {
-                    const close = wrapper.container.querySelector('[role="button"]');
-
-                    fireEvent.click(close);
-                });
-
-                it('Then does not show anything', async () => {
-                    await waitFor(() => {
-                        expect(wrapper.container.querySelector('.ui-LayoutBox')).toBeNull();
-                    });
-                });
-
-                it('Then calls onClose', () => {
-                    expect(props.onClose).toHaveBeenCalled();
-                });
-            });
-        });
-
-        describe('And autoclose is set', () => {
-            beforeEach(() => {
-                wrapper = render(
-                    <Alert {...props} autoclose autocloseDuration={1000}>
-                        Content
-                    </Alert>,
-                    {
-                        wrapper: Providers,
-                    }
-                );
-            });
-
-            afterAll(() => {
-                jest.clearAllMocks();
-            });
-
-            it('Then does not show anything', async () => {
-                await waitFor(() => {
-                    expect(wrapper.container.querySelector('.ui-LayoutBox')).toBeNull();
-                });
-            });
-
-            it('Then calls onClose', () => {
-                expect(props.onClose).toHaveBeenCalled();
-            });
-        });
-
-        describe('And it has an icon specified', () => {
-            it('Then does not show other icons', () => {
-                wrapper.rerender(
-                    <Alert {...props} icon='Icon'>
-                        Content
-                    </Alert>
-                );
-
-                expect(wrapper.container.querySelector('.ui-Alert__icon').innerHTML).toEqual('Icon');
+            it('Then contains the correct content', async () => {
+                expect(wrapper.container.innerHTML).toEqual('');
             });
         });
     });
