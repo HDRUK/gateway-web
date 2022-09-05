7a8c85091ae4d612a51c963b410b1cd103bb4b10
Geeta <geeta.kotecha@gmail.com>
2022-09-05 17:13:59 +0100

    working on saved preferences button and modal

--- src/pages/search/components/FilterSelection.js
+++ src/pages/search/components/FilterSelection.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from 'react';
 import FilterChip from './FilterChip';
 import { FilterCount } from './FilterCount';
 import SVGIcon from '../../../images/SVGIcon';
+import { Box } from 'hdruk-react-core';
 
 const FilterSelection = ({ selectedCount, selectedItems, onHandleClearSelection, onHandleClearAll, savedSearches }) => {
     const clearSelection = e => {
@@ -10,9 +11,12 @@ const FilterSelection = ({ selectedCount, selectedItems, onHandleClearSelection,
     };
 
     const [closed, setClosed] = useState(true);
+    const [shouldShowSavedPreferencesModal, setShouldShowSavedPreferenceModal] = useState(false)
 
     return (
         <Fragment>
+            <Box display="flex">
+            <Box flexGrow="1">
             <div className={savedSearches ? 'filters saved-filters' : 'filters'}>
                 <div className='filters-header'>
                     <div className={savedSearches ? 'filters-title black-16-semibold' : 'filters-title gray500-13'}>
@@ -61,6 +65,42 @@ const FilterSelection = ({ selectedCount, selectedItems, onHandleClearSelection,
                     </div>
                 ) : null}                
             </div>
+            </Box>
+            <Box display="inline-flex" alignItems="flex-end" justifyContent="flex-end">
+                <Button
+                variant='tertiary'
+                className='arrow'
+                aria-haspopup='true'
+                onClick={
+                    this.state.userState[0].loggedIn === false
+                        ? () => this.showLoginModal()
+                        : () => this.showSavedPreferencesModal(true)
+                }>
+                Save
+                <SVGIcon
+                    width='35px'
+                    height='35px'
+                    name='arrow-down'
+                    fill='#3C3C3B'
+                    className={this.state.closed ? '' : 'flip180'}
+                />{props.children}
+            </Button> 
+                    {<SavedPreferencesModal
+                        show={this.state.shouldShowSavedPreferencesModal}
+                        onHide={this.hideSavedPreferencesModal}
+                        viewMatchesLink={this.viewMatches}
+                        viewSaved={this.saveFiltersUpdate}
+                        activeTab={key}
+                        saveSuccess={this.showSuccessMessage}
+                        saveName={this.showSavedName}
+                        search={this.state.search}
+                        filters={preferenceFilters}
+                        sort={perferenceSort}
+                        tab={this.state.key}
+                        />
+                    }
+            </Box>
+            </Box>            
         </Fragment>
     );
 };
