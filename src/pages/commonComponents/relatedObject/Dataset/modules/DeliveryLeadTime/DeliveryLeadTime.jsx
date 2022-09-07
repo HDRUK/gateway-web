4f7e98fdf20ed75d13cabeab64c143d467554123
kymmeh <kymmehayley@gmail.com>
2022-09-07 11:34:51 +0100

    Updating styling

--- src/pages/commonComponents/relatedObject/Dataset/modules/DeliveryLeadTime/DeliveryLeadTime.jsx
+++ src/pages/commonComponents/relatedObject/Dataset/modules/DeliveryLeadTime/DeliveryLeadTime.jsx
@@ -4,15 +4,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { capitalize } from 'lodash';
 import { Box, Caption } from 'hdruk-react-core';
-import * as styles from './DeliveryLeadTime.styles';
 
 const DeliveryLeadTime = ({ deliveryLeadTime, label }) => {
     return (
-        <Box display='flex' justifyContent='start' css={styles.typicalTimeToAccess}>
+        <Box display='flex' justifyContent={{ sm: 'start', md: 'end' }}>
             <Caption color='grey600' mr='1'>
                 {label}
             </Caption>
-            <Caption color='grey600' css={styles.deliveryLeadTime}>
+            <Caption weight='bold' color='grey600'>
                 {capitalize(deliveryLeadTime)}
             </Caption>
         </Box>
