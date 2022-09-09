29990e5fdcb483671fd68150bf85e5bfbfcaab00
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-09-09 09:53:18 +0100

    Change default sort order to popularity

--- src/pages/search/SearchPage.js
+++ src/pages/search/SearchPage.js
@@ -458,7 +458,7 @@ class SearchPage extends React.Component {
             ? this.setState({ collectionIndex: queryParams.collectionIndex })
             : this.setState({ collectionIndex: 0 });
         // Sort for each tab
-        queryParams.datasetSort ? this.setState({ datasetSort: queryParams.datasetSort }) : this.setState({ datasetSort: '' });
+        queryParams.datasetSort ? this.setState({ datasetSort: queryParams.datasetSort }) : this.setState({ datasetSort: 'popularity' });
         queryParams.toolSort ? this.setState({ toolSort: queryParams.toolSort }) : this.setState({ toolSort: '' });
         queryParams.dataUseRegisterSort
             ? this.setState({ dataUseRegisterSort: queryParams.dataUseRegisterSort })
