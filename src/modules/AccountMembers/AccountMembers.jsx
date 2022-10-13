7d61af59d56ca36fa3644b17bf5ce045b828365f
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-10-13 17:45:13 +0100

    Memo

--- src/modules/AccountMembers/AccountMembers.jsx
+++ src/modules/AccountMembers/AccountMembers.jsx
@@ -1,33 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import axios from 'axios';
-import { Row, Col, Table } from 'react-bootstrap';
 import { isEmpty } from 'lodash';
-import { Card, CardBody, Typography, Button, CardHeader, P, Box, Caption } from 'hdruk-react-core';
+import { Card, CardBody, Typography, Button, CardHeader, P, Box } from 'hdruk-react-core';
 import { Link } from 'react-router-dom';
-import { ROLE_LIST } from '../../configs/constants';
+import { useTranslation } from 'react-i18next';
+import Table from '../../components/Table';
+import { ROLE_LIST, SUPPORT_URL } from '../../configs/constants';
 import MessageNotFound from '../../pages/commonComponents/MessageNotFound';
 import Loading from '../../pages/commonComponents/Loading';
-// import '../../css/styles.scss';
-// import './Dashboard.scss';
 import AccountMembersModal from '../AccountMembersModal';
 import { LayoutContent } from '../../components/Layout';
 import { baseURL } from '../../configs/url.config';
+import { useAuth } from '../../context/AuthContext';
 
-export const AccountMembers = props => {
-    const [userState] = useState(props.userState);
+export const AccountMembers = ({ team }) => {
+    const { userState } = useAuth();
     const [isLoading, setIsLoading] = useState(true);
     const [members, setMembers] = useState([]);
     const [userIsManager, setUserIsManager] = useState(false);
     const [showAccountAddMemberModal, setShowAccountAddMemberModal] = useState(false);
-    const [accountMembersId, setAccountMembersId] = useState(props.team);
+    const [accountMembersId, setAccountMembersId] = useState(team);
+    const { t } = useTranslation();
+
+    const columns = useMemo(
+        () => [
+            {
+                Header: 'Name',
+                accessor: 'name',
+            },
+            {
+                Header: 'Role',
+                accessor: 'role',
+            },
+        ],
+        []
+    );
 
     useEffect(() => {
-        setAccountMembersId(props.team);
-    }, [props]);
-
-    useEffect(() => {
-        doMembersCall().catch(console.error);
-    }, [accountMembersId]);
+        setAccountMembersId(team);
+    }, [team]);
 
     const doMembersCall = async () => {
         if (accountMembersId) {
@@ -35,18 +46,22 @@ export const AccountMembers = props => {
             await axios.get(`${baseURL}/api/v1/teams/${accountMembersId}/members`).then(async res => {
                 setMembers(res.data.members);
                 // TODO: GAT-1510:042
-                setUserIsManager(res.data.members.filter(m => m.id === userState[0].id).map(m => m.roles[0] === 'manager')[0]);
+                setUserIsManager(res.data.members.filter(({ id }) => id === userState[0].id).map(({ roles }) => roles[0] === 'manager')[0]);
             });
         }
         setIsLoading(false);
     };
 
+    useEffect(() => {
+        doMembersCall().catch(console.error);
+    }, [accountMembersId]);
+
     const onShowAccountMembersModal = () => {
         setShowAccountAddMemberModal(!showAccountAddMemberModal);
     };
 
-    const onMemberAdded = members => {
-        setMembers(members);
+    const onMemberAdded = addedMembers => {
+        setMembers(addedMembers);
     };
 
     const getRoles = roles => {
@@ -84,16 +99,10 @@ export const AccountMembers = props => {
                                 }}
                                 flexGrow='1'>
                                 <P mb={6}>
-                                    To remove team members or change their roles, please raise a support ticket at the following link:{' '}
-                                    <a href='https://hdruk.atlassian.net/servicedesk/customer/portal/1'>
-                                        https://hdruk.atlassian.net/servicedesk/customer/portal/1
-                                    </a>
+                                    {t('components.AccountMembers.members.description1')}: <a href={SUPPORT_URL}>{SUPPORT_URL}</a>
                                 </P>
-                                <P mb={6}>
-                                    Managers can; manage members, create and assign workflows, review applications that are assigned to them
-                                    and make the final decision on data access request applications.
-                                </P>
-                                <P>Reviewers can review applications that are assigned to them.</P>
+                                <P mb={6}>{t('components.AccountMembers.members.description2')}</P>
+                                <P>{t('components.AccountMembers.members.description3')}</P>
                             </Box>
                             <Box
                                 display={{
@@ -103,47 +112,35 @@ export const AccountMembers = props => {
                                 flexBasis={{
                                     md: '40%',
                                 }}>
-                                <Button variant='primary' onClick={onShowAccountMembersModal}>
-                                    + Add a new member
-                                </Button>
+                                {userIsManager && (
+                                    <Button variant='primary' onClick={onShowAccountMembersModal}>
+                                        + Add a new member
+                                    </Button>
+                                )}
                             </Box>
                         </Box>
                     </CardBody>
                 </Card>
 
-                {members.length <= 0 && (
-                    <Row className='margin-right-15'>
-                        <MessageNotFound word='members' />
-                    </Row>
-                )}
+                {members.length <= 0 && <MessageNotFound word='members' />}
                 {members.length > 0 && (
-                    <Box backgroundColor='white'>
-                        <Table className='dashboard-results'>
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Role</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {members.map(m => {
-                                    return (
-                                        <tr key={m.id}>
-                                            <td>
-                                                <Typography as={Link} to={`/person/${m.id}`} color='purple500'>
-                                                    {m.firstname} {m.lastname}
-                                                </Typography>
-                                                <Typography color='grey600'>{m.organisation ? m.organisation : m.bio}</Typography>
-                                            </td>
-                                            <td>
-                                                <Caption>{getRoles(m.roles)}</Caption>
-                                            </td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </Table>
-                    </Box>
+                    <Card>
+                        <Table
+                            className='dashboard-results'
+                            columns={columns}
+                            data={members.map(({ lastname, firstname, id, bio, organisation, roles }) => ({
+                                name: (
+                                    <>
+                                        <Typography as={Link} to={`/person/${id}`} color='purple500'>
+                                            {firstname} {lastname}
+                                        </Typography>
+                                        <Typography color='grey600'>{organisation || bio}</Typography>
+                                    </>
+                                ),
+                                role: getRoles(roles),
+                            }))}
+                        />
+                    </Card>
                 )}
 
                 <AccountMembersModal
