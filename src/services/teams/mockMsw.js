f3b58a6bc8d2306f48c4a536579c1c8679452e8b
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-10-17 15:00:33 +0100

    Change get members mock data slightly

--- src/services/teams/mockMsw.js
+++ src/services/teams/mockMsw.js
@@ -3,7 +3,12 @@ import { teamMembersMock } from '../../../test/mocks/teamsServiceMock';
 import { apiURL } from '../../configs/url.config';
 
 const mswGetMembers = rest.get(`${apiURL}/teams/5f7b1a2bce9f65e6ed83e7da/members`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(teamMembersMock));
+    return res(
+        ctx.status(200),
+        ctx.json({
+            members: teamMembersMock,
+        })
+    );
 });
 
 export default [mswGetMembers];
