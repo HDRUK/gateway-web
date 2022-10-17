b309847c6eeab0199896b619fad4fec3350c345a
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-10-17 10:57:47 +0100

    Members title

---
+++ src/consts/support.js
@@ -0,0 +1,2 @@
+export const SUPPORT_CREATE_URL = 'https://hdruk.atlassian.net/servicedesk/customer/portal/1/group/1/create/1';
+export const SUPPORT_URL = 'https://hdruk.atlassian.net/servicedesk/customer/portal/1';
