040d8af86b30fe6f4e1d209b18f100daa68c9379
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-10-17 17:11:08 +0100

    Fake checkboxes and ActionCard

---
+++ src/components/ActionCard/ActionCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Card, CardBody, H5 } from 'hdruk-react-core';
+
+const ActionCard = ({ title, content, action, ...outerProps }) => (
+    <Card {...outerProps}>
+        <CardBody>
+            <Box
+                display={{
+                    md: 'flex',
+                }}
+                gap={8}>
+                <Box flexGrow='1'>
+                    <H5 mb={1}>{title}</H5>
+                    {content}
+                </Box>
+                {action && (
+                    <Box
+                        display={{
+                            md: 'flex',
+                        }}
+                        justifyContent='flex-end'
+                        alignItems='center'
+                        flexBasis={{
+                            md: '40%',
+                        }}>
+                        {action}
+                    </Box>
+                )}
+            </Box>
+        </CardBody>
+    </Card>
+);
+
+ActionCard.propTypes = {
+    title: PropTypes.node,
+    action: PropTypes.node,
+    content: PropTypes.node,
+};
+
+ActionCard.defaultProps = {
+    title: null,
+    content: null,
+    action: null,
+};
+
+export default ActionCard;
--- src/components/Checkbox/Checkbox.styles.js
+++ src/components/Checkbox/Checkbox.styles.js
@@ -14,6 +14,7 @@ export const root =
             ${mixins.root({ width, disabled })}
 
             font-size: ${fontSize};
+            display: block;
 
             &::before {
                 ${mixins.before({ colors, variants, variant, width, height })}
