8ac4d9274003fe8bd9b9c34da2d32d48e3426638
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-10-17 13:56:23 +0100

    Move response mock data

---
+++ test/mocks/teamsServiceMock.js
@@ -0,0 +1,24 @@
+const __teamMembers = {
+    members: [
+        {
+            firstname: 'John',
+            lastname: 'Smith',
+            id: '5678',
+            roles: ['manager'],
+            organisation: 'HDR UK',
+            bio: 'Developer',
+        },
+        {
+            firstname: 'Jane',
+            lastname: 'Doe',
+            id: '5678',
+            roles: ['manager'],
+            organisation: 'Google',
+            bio: 'Manager',
+        },
+    ],
+};
+
+module.exports = {
+    teamMembersMock: __teamMembers,
+};
