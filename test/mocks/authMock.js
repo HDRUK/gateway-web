f30365f5e39ac3273c52fb9e2dd473b17f490d76
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-10-21 10:23:11 +0100

    Remove file

--- test/mocks/authMock.js
+++
@@ -1,9 +0,0 @@
-const authMock = jest.mock('../../src/context/AuthContext', () => ({
-    ...jest.requireActual('../../src/context/AuthContext'),
-    useAuth: jest.fn().mockReturnValue({
-        isTeamManager: false,
-        managerInTeam: jest.fn(),
-    }),
-}));
-
-export { authMock };
