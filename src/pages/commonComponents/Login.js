3c1e3b7eed4a1ca190eb618aca4f70ceca3b87d2
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-11-04 09:20:14 +0000

    Spacing

--- src/pages/commonComponents/Login.js
+++ src/pages/commonComponents/Login.js
@@ -1,21 +1,21 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { Row, Col, Image } from 'react-bootstrap';
-import { Box, Icon, Message } from 'hdruk-react-core';
-import { t } from 'react-i18next';
+import { Box, Icon, Li, Message, P, Ul } from 'hdruk-react-core';
+import { useTranslation } from 'react-i18next';
 import LayoutContent from 'components/Layout/LayoutContent';
 import LoginSSOButtons from './LoginSSOButtons/index';
 import { ReactComponent as TickSvg } from '../../images/tick.svg';
 import { ssoBtnsConfig } from '../../configs/ssoBtnsConfig';
 
 import './CommonComponents.scss';
+import Loading from './Loading';
+import { URL_OIDC } from '../../configs/constants';
 
 const baseURL = require('./BaseURL').getURL();
 const communityLink = require('./BaseURL').getDiscourseURL();
 
 function Login() {
-    const [showButtons, setShowButtons] = useState(true);
-    const [showWayFinder, setShowWayFinder] = useState();
-
+    const { t } = useTranslation();
     const lastChoice = localStorage.getItem('lastChoice');
 
     const descText = [
@@ -30,78 +30,73 @@ function Login() {
         },
     ];
 
-    const handleShowWayFinder = () => {
-        setShowButtons(false);
-        setShowWayFinder(true);
+    const showWayFinder = e => {
+        document.getElementById('loginWayFinder').style.display = 'block';
+        document.getElementById('loginButtons').style.display = 'none';
     };
 
-    const handleHideWayFinder = () => {
-        setShowButtons(true);
-        setShowWayFinder(false);
+    const hideWayFinder = e => {
+        document.getElementById('loginButtons').style.display = 'block';
+        document.getElementById('loginWayFinder').style.display = 'none';
     };
 
     const clickHandler = (id, authURL) => {
         localStorage.setItem('lastChoice', id);
 
         if (id === 'openAthens') {
-            handleShowWayFinder();
-        } else {
-            window.location.href = `${baseURL}${authURL}`;
+            return showWayFinder();
         }
+
+        window.location.href = `${baseURL}${authURL}`;
     };
 
     return (
         <Box mb={1}>
-            <div>
+            <div id='loginButtons'>
                 <LayoutContent>
-                    <Message mt={2}>{t('components.Login.description')}:</Message>
-                    {descText.map(value => (
-                        <Message mt={2} key={value.text}>
-                            <Icon svg={<TickSvg />} /> {value.text}
-                        </Message>
-                    ))}
+                    <P mt={2} mb={4} color='grey800'>
+                        {t('components.Login.description')}:
+                    </P>
+                    <Ul mb={6}>
+                        {descText.map(({ text }) => (
+                            <Li icon={<Icon svg={<TickSvg />} size='xl' />}>{text}</Li>
+                        ))}
+                    </Ul>
+                    <P mb={2} color='grey800'>
+                        {t('components.Login.descriptionButtons')}
+                    </P>
                 </LayoutContent>
 
-                {showButtons && (
-                    <LoginSSOButtons
-                        clickHandler={clickHandler}
-                        communityLink={communityLink}
-                        lastChoice={lastChoice}
-                        ssoBtnsConfig={ssoBtnsConfig}
-                    />
-                )}
+                <LoginSSOButtons
+                    clickHandler={clickHandler}
+                    communityLink={communityLink}
+                    lastChoice={lastChoice}
+                    ssoBtnsConfig={ssoBtnsConfig}
+                />
             </div>
 
-            {showWayFinder && (
-                <div>
-                    <Box mt={3} textAlign='center'>
-                        <LayoutContent>
-                            <a href='javascript:void(0)' onClick={handleHideWayFinder} className='purple-14'>
-                                Show all login options
-                            </a>
-                        </LayoutContent>
-                    </Box>
-                    <Row className='mt-4'>
-                        <Col sm={1} lg={1} />
-                        <Col sm={10} lg={10}>
-                            <div id='wayfinder'>
-                                <div className='gray800-14' style={{ textAlign: 'center' }}>
-                                    <Image src={require('../../images/Loader.gif')} />
-                                </div>
-                                <div className='gray800-14' style={{ textAlign: 'center' }}>
-                                    Loading...
-                                    <br />
-                                    <br />
-                                    <a href={`${baseURL}/auth/oidc`} className='purple-14'>
-                                        Click here if login screen does not load
+            <div id='loginWayFinder' style={{ display: 'none' }}>
+                <Box mt={3} textAlign='center'>
+                    <LayoutContent>
+                        <a href='javascript:void(0)' onClick={hideWayFinder} className='purple-14'>
+                            {t('components.Login.linkLoginOptions')}
+                        </a>
+                    </LayoutContent>
+                </Box>
+                <Box mt={3}>
+                    <LayoutContent>
+                        <div id='wayfinder'>
+                            <Loading
+                                subText={
+                                    <a href={URL_OIDC} className='purple-14'>
+                                        {t('components.Login.notLoading')}
                                     </a>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col sm={1} lg={1} />
-                    </Row>
-                </div>
-            )}
+                                }
+                            />
+                        </div>
+                    </LayoutContent>
+                </Box>
+            </div>
         </Box>
     );
 }
