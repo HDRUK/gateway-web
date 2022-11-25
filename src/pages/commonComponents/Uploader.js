5ab94edbb52eb5c76f6051d49995f93f46113239
kymmeh <kymmehayley@gmail.com>
2022-11-25 13:52:59 +0000

    Removing import

--- src/pages/commonComponents/Uploader.js
+++ src/pages/commonComponents/Uploader.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import { ReactComponent as PersonPlaceholderSvg } from '../../images/person-placeholder.svg';
 import './CommonComponents.scss';
