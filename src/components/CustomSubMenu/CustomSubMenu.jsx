745639c82b878945ff3f9b0f860f40ec2945d667
kymmeh <kymmehayley@gmail.com>
2023-01-25 10:40:55 +0000

    Removing fragment

--- src/components/CustomSubMenu/CustomSubMenu.jsx
+++ src/components/CustomSubMenu/CustomSubMenu.jsx
@@ -1,15 +1,13 @@
-import { forwardRef, useState, Children, Fragment } from 'react';
+import { forwardRef, useState, Children } from 'react';
 import PropTypes from 'prop-types';
 
-const CustomSubMenu = forwardRef(({ children, style, className, show, 'aria-labelledby': labeledBy }, ref) => {
+const CustomSubMenu = forwardRef(({ children, show, 'aria-labelledby': labeledBy }, ref) => {
     const [value] = useState('');
     if (show) {
         return (
-            <Fragment ref={ref} style={style} className={className} aria-labelledby={labeledBy}>
-                <ul className='list-unstyled'>
-                    {Children.toArray(children).filter(child => !value || child.props.children.toLowerCase().startsWith(value))}
-                </ul>
-            </Fragment>
+            <ul ref={ref} className='list-unstyled' aria-labelledby={labeledBy}>
+                {Children.toArray(children).filter(child => !value || child.props.children.toLowerCase().startsWith(value))}
+            </ul>
         );
     }
     return null;
@@ -18,8 +16,6 @@ const CustomSubMenu = forwardRef(({ children, style, className, show, 'aria-labe
 CustomSubMenu.propTypes = {
     show: PropTypes.bool.isRequired,
     children: PropTypes.node.isRequired,
-    className: PropTypes.string.isRequired,
-    style: PropTypes.string.isRequired,
     'aria-labelledby': PropTypes.string.isRequired,
 };
 
