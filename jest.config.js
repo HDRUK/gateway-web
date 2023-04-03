b1048aead112860b9741d7d4438720b838fbc2f7
Rabi Sahoo <Rabi@HDR-MacBook-3.local>
2023-04-03 18:39:14 +0100

    added more configuration for testing

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,25 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/coverage', '<rootDir>/dist'],
+  moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/src'],
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@pages/(.*)': '<rootDir>/src/pages/$1',
+    '@components/(.*)': '<rootDir>/src/components/$1',
+    '@styles/(.*)': '<rootDir>/src/styles/$1',
+    '\\.(scss|sass|css)': 'identity-obj-proxy',
+  },
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
