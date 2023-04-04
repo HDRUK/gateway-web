eb389da39071af437d0f97907691464f713a04d4
kymmeh <kymmehayley@gmail.com>
2023-04-04 10:05:22 +0100

    Move test / update type

--- src/services/api/get.ts
+++ src/services/api/get.ts
@@ -1,6 +1,9 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-const getRequest = async <T>(url: string, options?: any): Promise<T> => {
+const getRequest = async <T>(
+	url: string,
+	options?: AxiosRequestConfig
+): Promise<T> => {
 	return await axios.get(url, options).then((res) => res.data);
 };
 
