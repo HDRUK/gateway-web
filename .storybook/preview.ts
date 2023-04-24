6db24d5c5dd1e8602f456d3d7410ed3cb14e8cb3
Rabi Sahoo <Rabi@HDR-MacBook-3.local>
2023-04-24 16:08:45 +0100

    Added alphabetical sorting for stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,11 +3,11 @@ import type { Preview } from "@storybook/react";
 const preview: Preview = {
     parameters: {
         actions: { argTypesRegex: "^on[A-Z].*" },
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/,
-            },
+        options: {
+            storySort: (a, b) =>
+                a.id === b.id
+                    ? 0
+                    : a.id.localeCompare(b.id, undefined, { numeric: true }),
         },
     },
 };
