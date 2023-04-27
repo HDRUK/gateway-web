4e2581b662577fd7b479ec58a8a0391cc4804c47
kymmeh <kymmehayley@gmail.com>
2023-04-27 11:40:33 +0100

    Adding cross

--- src/modules/AccountTeamNotificationsConfirmationModal/AccountTeamNotificationsConfirmationModal.jsx
+++ src/modules/AccountTeamNotificationsConfirmationModal/AccountTeamNotificationsConfirmationModal.jsx
@@ -5,14 +5,18 @@ import PropTypes from 'prop-types';
 import '../AccountTeamEmailAlertModal/AccountTeamEmailAlertModal.scss';
 import { teamNotificationsPropTypes } from 'types';
 import { EmailList } from './AccountTeamNotificationsConfirmationModal.components';
+import { ReactComponent as CloseButtonSvg } from '../../images/close-alt.svg';
 
 const AccountTeamNotificationsConfirmationModal = ({ isOpen, onClose, onConfirm, teamNotifications }) => {
     const { t } = useTranslation();
 
     return (
         <Modal show={isOpen} onHide={() => onClose(false)}>
-            <ModalBody>
+            <div className='teamEmailAlertModal-header'>
                 <H5 mb={2}>{t('components.AccountTeamNotificationsConfirmationModal.title')}</H5>
+                <CloseButtonSvg className='teamEmailAlertModal-header--close' onClick={() => onClose(false)} />
+            </div>
+            <ModalBody>
                 <P>{t('components.AccountTeamNotificationsConfirmationModal.description')}</P>
                 <EmailList notifications={teamNotifications} />
             </ModalBody>
