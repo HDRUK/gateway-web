32870979082fbdbbaa0f255e7b8c043998e5f146
kymmeh <kymmehayley@gmail.com>
2023-05-23 09:28:43 +0100

    Adding tests

--- __tests__/components/Dialog.test.tsx
+++ __tests__/components/Dialog.test.tsx
@@ -1,5 +1,24 @@
+import Dialog from "@/components/Dialog";
+import { fireEvent, render, screen, waitFor } from "../testUtils";
+
 describe("Dialog", () => {
-    it("should", () => {
-        expect(true).toBeTruthy();
+    it("should render title / content", async () => {
+        render(<Dialog title="Dialog title">Dialog content</Dialog>);
+        expect(await screen.getByText("Dialog title")).toBeInTheDocument();
+        expect(await screen.getByText("Dialog content")).toBeInTheDocument();
+    });
+    it("should render title / content", async () => {
+        const closeMock = jest.fn();
+        render(
+            <Dialog onClose={closeMock} title="Dialog title">
+                Dialog content
+            </Dialog>
+        );
+
+        fireEvent.click(screen.getByTestId("dialog-close-icon"));
+
+        await waitFor(() => {
+            expect(closeMock).toHaveBeenCalled();
+        });
     });
 });
