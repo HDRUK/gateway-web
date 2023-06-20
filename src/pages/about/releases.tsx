df144a98ed60c7ea3a22ea442e840c4f1c177068
kymmeh <kymmehayley@gmail.com>
2023-06-20 22:34:37 +0100

    Testing release page

--- src/pages/about/releases.tsx
+++ src/pages/about/releases.tsx
@@ -2,13 +2,13 @@ import Head from "@/components/Head";
 import { loadServerSideLocales } from "@/utils/locale";
 import Container from "@/components/Container";
 import { getReleaseNotes } from "@/utils/cms";
-import { ReleaseNotesResponse } from "@/interfaces/Releases";
+import { ReleaseNode } from "@/interfaces/Releases";
 import ReleaseTabs from "@/modules/ReleaseTabs";
 import Banner from "@/components/Banner";
 import BannerImage from "../../../public/images/banners/release-notes.png";
 
 interface ReleasesProps {
-    allReleases: ReleaseNotesResponse;
+    allReleases: ReleaseNode[];
 }
 
 const Releases = ({ allReleases }: ReleasesProps) => {
