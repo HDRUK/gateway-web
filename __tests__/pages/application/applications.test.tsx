0f5c3cd20e80463d46de3953395c1f4b75f5bf96
Sorin Gumeni <sorin.gumeni@hdruk.ac.uk>
2023-07-18 17:34:06 +0100

    GAT-2588: Add build and deploy pipeline

--- __tests__/pages/application/applications.test.tsx
+++ __tests__/pages/application/applications.test.tsx
@@ -2,14 +2,25 @@ import React from "react";
 import Application from "@/pages/account/application";
 import { render, screen, waitFor } from "../../testUtils";
 
+
+jest.mock('next/router', () => ({
+    useRouter() {
+      return ({
+        route: '/',
+        pathname: '',
+        query: '',
+        asPath: '',
+      });
+    },
+  }));
+
 describe("Applications", () => {
     it("should render contents", async () => {
         render(
             <Application />
         );
-
         await waitFor(() => {
-            expect(screen.getByText("Public app name")).toBeInTheDocument();
+            expect(screen.getByText("Application Management")).toBeInTheDocument();
             expect(screen.queryByText("Placeholder for Auth Tab")).not.toBeInTheDocument();
             expect(screen.queryByText("Placeholder for Scopes/Permissions Tab")).not.toBeInTheDocument();
         });
