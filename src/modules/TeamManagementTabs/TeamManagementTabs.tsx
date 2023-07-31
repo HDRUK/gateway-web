b4d3cc4eb7b53ce28877cfa4c3a7d76651d74afe
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2023-07-31 09:44:38 +0100

    adds in team management page ready for next stories

---
+++ src/modules/TeamManagementTabs/TeamManagementTabs.tsx
@@ -0,0 +1,45 @@
+import Tabs from "@/components/Tabs";
+import { Typography } from "@mui/material";
+import { useState } from "react";
+
+const tabsList = [ 
+    {
+        label: "Members",
+        value: "Members",
+        content: (
+            <Typography component="span">
+                Placeholder for Members Tab
+            </Typography>
+        ),
+    }, 
+    {
+        label: "Notifications",
+        value: "Notifications",
+        content: (
+            <Typography component="span">
+                Placeholder for Notifications Tab
+            </Typography>
+        ),
+    },
+];
+
+const TeamManagementTabs = () => {
+    const [selectedTab, setSelectedTab] = useState("Members");
+
+    const handleTabChange = (tab: string) => {
+        setSelectedTab(tab);
+    };
+
+    return (
+        <Tabs
+            centered
+            value={selectedTab}
+            onChange={handleTabChange}
+            tabs={tabsList}
+            tabBoxSx={{ padding: 0 }}
+            rootBoxSx={{ padding: 0 }}
+        />
+    );
+};
+
+export default TeamManagementTabs;
