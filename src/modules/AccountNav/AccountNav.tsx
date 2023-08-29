8621219c684424271128517d12275f7e72028ed7
kymmeh <kymmehayley@gmail.com>
2023-08-29 16:19:19 +0100

    Updating nav design

--- src/modules/AccountNav/AccountNav.tsx
+++ src/modules/AccountNav/AccountNav.tsx
@@ -2,10 +2,8 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Link from "@/components/Link";
 import Button from "@/components/Button";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import useAuth from "@/hooks/useAuth";
-
-import { useEffect } from "react";
+import { colors } from "@/config/theme";
 
 interface AccountNavProps {
     onCloseMenu: () => void;
@@ -18,11 +16,10 @@ const AccountNav = ({
     onCloseMenu,
     onLogout,
 }: AccountNavProps) => {
-    let links = [
-        { id: -99, label: "Profile", href: "/account/profile" },
-    ];
-
     const { user } = useAuth();
+    const links = [
+        { id: -99, label: `${user?.firstname} team`, href: "/account/profile" },
+    ];
 
     const handleCloseUserMenu = () => {
         if (typeof onCloseMenu === "function") {
@@ -36,14 +33,12 @@ const AccountNav = ({
         }
     };
 
-    useEffect(() => {
-        if (!user) {
-            return;
-        }
-    }, [user]);
-
-    user.teams.map(team => {
-        let t = { id: team.id, label: team.name, href: `/account/team/${team.id}` };
+    user.teams.forEach(team => {
+        const t = {
+            id: team.id,
+            label: team.name,
+            href: `/account/team/${team.id}`,
+        };
 
         if (!links.includes(t)) {
             links.push(t);
@@ -52,51 +47,28 @@ const AccountNav = ({
 
     return (
         <Menu
-            PaperProps={{
-                elevation: 0,
-                sx: {
-                    overflow: "visible",
-                    mt: 1.5,
-                    "& .MuiAvatar-root": {
-                        width: 32,
-                        height: 32,
-                        ml: -0.5,
-                        mr: 1,
-                    },
-                    "&:before": {
-                        content: '""',
-                        display: "block",
-                        position: "absolute",
-                        top: 0,
-                        right: 14,
-                        width: 10,
-                        height: 10,
-                        bgcolor: "background.paper",
-                        transform: "translateY(-50%) rotate(45deg)",
-                        zIndex: 0,
-                    },
-                },
-            }}
             id="account-nav"
             anchorEl={anchorElement}
-            transformOrigin={{ horizontal: "right", vertical: "top" }}
             anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
             onClose={handleCloseUserMenu}
             open={Boolean(anchorElement)}>
             {links.map(link => (
                 <Link key={link.label} underline="hover" href={link.href}>
                     <MenuItem
-                        sx={{ width: 220 }}
+                        sx={{
+                            textWrap: "initial",
+                            width: 207,
+                            borderBottom: `${colors.grey300} 1px solid`,
+                        }}
                         key={link.label}
                         LinkComponent={Link}
                         onClick={handleCloseUserMenu}>
-                        <ChevronLeftIcon />
                         {link.label}
                     </MenuItem>
                 </Link>
             ))}
             <Button variant="link">
-                <MenuItem sx={{ width: 220 }} onClick={handleLogout}>
+                <MenuItem sx={{ width: 207 }} onClick={handleLogout}>
                     Logout
                 </MenuItem>
             </Button>
