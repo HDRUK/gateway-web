686f44fcb312533536adf13211bca572bd3250b2
kymmeh <kymmehayley@gmail.com>
2023-09-22 10:35:29 +0100

    update role name

--- src/consts/roles.ts
+++ src/consts/roles.ts
@@ -1,7 +1,7 @@
 const ROLE_CUSTODIAN_DEVELOPER = "developer";
 const ROLE_CUSTODIAN_TEAM_ADMIN = "custodian.team.admin";
 const ROLE_CUSTODIAN_METADATA_MANAGER = "custodian.metadata.manager";
-const ROLE_CUSTODIAN_METADATA_EDITOR = "metadata_editor";
+const ROLE_CUSTODIAN_METADATA_EDITOR = "metadata.editor";
 const ROLE_CUSTODIAN_DAR_MANAGER = "custodian.dar.manager";
 const ROLE_CUSTODIAN_DAR_REVIEWER = "reviewer";
 
