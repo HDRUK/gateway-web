5b9b665cc688c4d9bba9b159cfc1bd6a9c25ea2c
Calum Macdonald <calmacx@gmail.com>
2023-10-12 14:18:30 +0100

    missed one

--- src/components/CopyTextButton/CopyTextButton.test.tsx
+++ src/components/CopyTextButton/CopyTextButton.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@/utils/testUtils";
 import CopyTextButton from "./CopyTextButton";
 
-describe("CopyableTextBox", () => {
+describe("CopyTextButton", () => {
     it("should render component", async () => {
         render(<CopyTextButton content={"content to copy"}/>);
         expect(screen.getByText("content to copy")).toBeInTheDocument();
