2d5d0bb5df36782fcedc8ee856e3631155f9c4e9
Calum Macdonald <calmacx@gmail.com>
2023-10-12 14:19:41 +0100

    remove whitespace changes

--- src/utils/general.test.tsx
+++ src/utils/general.test.tsx
@@ -4,5 +4,4 @@ describe("General utils", () => {
     it("should return capitalised string", async () => {
         expect(capitalise("string")).toEqual("String");
     });
-
 });
--- src/utils/general.ts
+++ src/utils/general.ts
@@ -2,5 +2,4 @@
 const capitalise = (str: string) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 };
-
 export { capitalise };
