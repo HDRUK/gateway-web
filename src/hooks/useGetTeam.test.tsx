fe8af930162f77f0d468159240e1c74945dc9701
kymmeh <kymmehayley@gmail.com>
2023-10-24 12:00:13 +0100

    Updating test descriptions

--- src/hooks/useGetTeam.test.tsx
+++ src/hooks/useGetTeam.test.tsx
@@ -3,7 +3,17 @@ import { renderHook, waitFor } from "@/utils/testUtils";
 import { teamV1 } from "@/mocks/data/team";
 
 describe("useGetTeam", () => {
-    it("should return the logged in user", async () => {
+    it("should return initial response", async () => {
+        const { result } = renderHook(() => useGetTeam(teamV1.id));
+
+        expect(result.current).toEqual({
+            error: undefined,
+            isTeamLoading: true,
+            team: undefined,
+            mutateTeam: expect.any(Function),
+        });
+    });
+    it("should return the loaded team", async () => {
         const { result } = renderHook(() => useGetTeam(teamV1.id));
         await waitFor(() => {
             expect(result.current).toEqual({
@@ -12,16 +22,6 @@ describe("useGetTeam", () => {
                 team: teamV1,
                 mutateTeam: expect.any(Function),
             });
-        });
-    });
-    it("should return logged out props", async () => {
-        const { result } = renderHook(() => useGetTeam(teamV1.id));
-
-        expect(result.current).toEqual({
-            error: undefined,
-            isTeamLoading: true,
-            team: undefined,
-            mutateTeam: expect.any(Function),
         });
     });
 });
