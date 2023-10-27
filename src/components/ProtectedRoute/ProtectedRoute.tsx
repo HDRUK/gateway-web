b012a6a7f0e13e9e15e79779cebf8fcd56858be3
Calum Macdonald <calmacx@gmail.com>
2023-10-27 15:28:02 +0100

    remove useEffect that isnt used

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode } from "react";
 import { useHasPermissions } from "@/hooks/useHasPermission";
 import Unauthorised from "@/components/Unauthorised";
 
