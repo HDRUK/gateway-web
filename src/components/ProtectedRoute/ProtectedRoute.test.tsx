3e247fa2abb29658908d30f0f7e7aabe4eb5d6d8
Calum Macdonald <calmacx@gmail.com>
2023-10-27 11:50:17 +0100

    add better testing for a protected route

--- src/components/ProtectedRoute/ProtectedRoute.test.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.test.tsx
@@ -1,14 +1,60 @@
 import React from "react";
 import ProtectedRoute from "@/components/ProtectedRoute";
+import { useRouter } from "next/router";
+import { useHasPermissions } from "@/hooks/useHasPermission";
 import { render, screen } from "@/utils/testUtils";
+import protectedRoutes from "@/config/protectedRoutes";
+
+jest.mock("next/router", () => ({
+    useRouter: jest.fn(),
+}));
+
+jest.mock("@/hooks/useHasPermission", () => ({
+    useHasPermissions: jest.fn(),
+}));
 
 describe("ProtectedRoute", () => {
-    it("should render component", async () => {
-        render(<b> test </b>);
-        /*<ProtectedRoute>
-                <b> test </b>
-            </ProtectedRoute>*/
+    it("should render the content ", async () => {
+        let permissions = {};
+        permissions[protectedRoutes[0].permissions[0]] = true;
+        useHasPermissions.mockImplementation(() => permissions);
 
-        expect(screen.getByText("test")).toBeInTheDocument();
+        useRouter.mockImplementation(() => ({
+            asPath: protectedRoutes[0].route,
+            route: protectedRoutes[0].route,
+            pathname: "",
+            query: {},
+        }));
+
+        render(
+            <ProtectedRoute>
+                <b> This is a protected route </b>
+            </ProtectedRoute>
+        );
+
+        expect(
+            screen.getByText("This is a protected route")
+        ).toBeInTheDocument();
+    });
+
+    it("should not render the content, but render a 401", async () => {
+        let permissions = {};
+        permissions[protectedRoutes[0].permissions[0]] = false;
+        useHasPermissions.mockImplementation(() => permissions);
+
+        useRouter.mockImplementation(() => ({
+            asPath: protectedRoutes[0].route,
+            route: protectedRoutes[0].route,
+            pathname: "",
+            query: {},
+        }));
+
+        render(
+            <ProtectedRoute>
+                <b> This is a protected route </b>
+            </ProtectedRoute>
+        );
+
+        expect(screen.getByText("401 Unauthorised")).toBeInTheDocument();
     });
 });
