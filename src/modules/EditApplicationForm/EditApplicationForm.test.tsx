7cdce2bc71b8fda7cd3348e34df034098c013d59
Calum Macdonald <calmacx@gmail.com>
2023-11-20 13:30:59 +0000

    improve testing

--- src/modules/EditApplicationForm/EditApplicationForm.test.tsx
+++ src/modules/EditApplicationForm/EditApplicationForm.test.tsx
@@ -1,6 +1,14 @@
+import { render, screen, within } from "@/utils/testUtils";
+import EditApplicationForm from "./EditApplicationForm";
+import { generateApplicationV1 } from "@/mocks/data/application";
+
 describe("EditApplicationForm", () => {
+    const mockApplication = generateApplicationV1();
+
     it("should render component", async () => {
-        // todo: add missing tests
-        expect(true).toBeTruthy();
+        render(<EditApplicationForm application={mockApplication} />);
+        expect(screen.getByText("Public API name")).toBeInTheDocument();
+        expect(screen.getByText("Description")).toBeInTheDocument();
+        expect(screen.getByText("Notification Contacts")).toBeInTheDocument();
     });
 });
