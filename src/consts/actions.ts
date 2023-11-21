4a609c22e641788169c331b7e113e381e1e53b77
Calum Macdonald <calmacx@gmail.com>
2023-11-21 15:08:15 +0000

    put actions in a seperate file

---
+++ src/consts/actions.ts
@@ -0,0 +1,10 @@
+import InfoIcon from "@mui/icons-material/Info";
+
+const nonManualActions = [
+    {
+        icon: InfoIcon,
+        label: "Datasets uploaded via API integration or federated integration can be managed via those methods. Only manually created datasets can be managed on this dataset management page. Contact your Team Administrator or Developer for information on the API or federated integration.",
+    },
+];
+
+export { nonManualActions };
