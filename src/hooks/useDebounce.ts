8d397e527d4add38e2957ada8788deda8154214f
Calum Macdonald <calmacx@gmail.com>
2023-11-27 16:12:18 +0000

    fix for min letters

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 
-const useDebounce = (value: any, delay: number = 500) => {
+const useDebounce = (
+    value: any,
+    delay: number = 500,
+    minLetters: number = 3
+) => {
     const [debouncedValue, setDebouncedValue] = useState(value);
 
     useEffect(() => {
+        if (value.length < minLetters && value.length > 0) return;
+
         const handler = setTimeout(() => {
             setDebouncedValue(value);
         }, delay);
