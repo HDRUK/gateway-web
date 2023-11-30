790856d5940b893140cf69894ff61e495c395db5
kymmeh <kymmehayley@gmail.com>
2023-11-30 16:13:49 +0000

    Adding navigation mocks

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,31 @@
 import "@testing-library/jest-dom/extend-expect";
 import { server } from "./mocks/server";
 
+const nextRouterMock = require("next-router-mock");
+
 require("jest-fetch-mock").enableMocks();
 
 // mock useRouter
-jest.mock("next/router", () => jest.requireActual("next-router-mock"));
+jest.mock("next/router", () => nextRouterMock);
+
+jest.mock("next/navigation", () => {
+    const { useRouter } = nextRouterMock;
+    const usePathname = () => {
+        const router = useRouter();
+        return router.pathname;
+    };
+
+    const useSearchParams = () => {
+        const router = useRouter();
+        return new URLSearchParams(router.query);
+    };
+
+    return {
+        useRouter,
+        usePathname,
+        useSearchParams,
+    };
+});
 
 jest.mock("react-i18next", () => ({
     // this mock makes sure any components using the translate hook can use it without a warning being shown
