a7b1a93645b4e397f7c59f8d177d6d73b20aa62b
Calum Macdonald <calmacx@gmail.com>
2023-12-08 14:23:33 +0000

    remove redundant

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -9,25 +9,19 @@ import locale from "dayjs/locale/en-gb";
 export interface DatePickerProps {
     label: string;
     name: string;
-    useCurrentDate?: boolean;
-    useStartDate?: string;
     info?: string;
     extraInfo?: string;
     required?: boolean;
     disabled?: boolean;
     horizontalForm?: boolean;
     isRow: boolean;
-    radios: { value: string; label: string }[];
     control: Control;
-    radioSx?: SxProps;
     formControlSx?: SxProps;
 }
 
 const DatePicker = ({
     label,
     formControlSx,
-    radioSx = {},
-    radios,
     control,
     name,
     horizontalForm = false,
