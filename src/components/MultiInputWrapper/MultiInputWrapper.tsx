eb3839eab21ab83bf731bdc62ae994ef2906e939
Calum Macdonald <calmacx@gmail.com>
2023-12-11 11:47:44 +0000

    adding better rendering

--- src/components/MultiInputWrapper/MultiInputWrapper.tsx
+++ src/components/MultiInputWrapper/MultiInputWrapper.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import InputWrapper from "@/components/InputWrapper";
+import { ComponentTypes } from "@/interfaces/ComponentTypes";
 import { Box } from "@mui/material";
 import { Control } from "react-hook-form";
 
@@ -9,57 +10,64 @@ export interface MultiInputWrapperProps {
     control: Control;
 }
 
+type FieldProps = {
+    name: string;
+    label: string;
+    fields?: FieldProps[];
+    component: ComponentTypes;
+};
+
 const MultiInputWrapper = ({ fields, control }: MultiInputWrapperProps) => {
+    const renderField = (field: FieldProps) => {
+        if (field.component) {
+            return (
+                <InputWrapper key={field.name} control={control} {...field} />
+            );
+        } else if (field.fields) {
+            return (
+                <>
+                    {field.label + ":"}
+                    <Box
+                        key={field.name}
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            py: 1,
+                            my: 1,
+                        }}>
+                        {field.fields.map(subField => (
+                            <>
+                                {subField.label}
+                                <InputWrapper
+                                    formControlSx={{
+                                        p: 0,
+                                        mx: 2,
+                                        my: "auto",
+                                    }}
+                                    key={subField.name}
+                                    control={control}
+                                    {...subField}
+                                    label={""}
+                                />
+                            </>
+                        ))}
+                    </Box>
+                </>
+            );
+        }
+    };
+
     return (
         <Box sx={{ p: 0 }}>
-            {fields.map(field => {
-                if (field.component) {
-                    return (
-                        <Box
-                            sx={{
-                                py: 1,
-                                my: 1,
-                            }}>
-                            <InputWrapper
-                                key={field.name}
-                                control={control}
-                                {...field}
-                            />
-                        </Box>
-                    );
-                } else if (field.fields) {
-                    return (
-                        <>
-                            {field.label + ":"}
-                            <Box
-                                key={field.name}
-                                sx={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    py: 1,
-                                    my: 1,
-                                }}>
-                                {field.fields.map(subField => (
-                                    <>
-                                        {subField.label}
-                                        <InputWrapper
-                                            formControlSx={{
-                                                p: 0,
-                                                mx: 2,
-                                                my: "auto",
-                                            }}
-                                            key={subField.name}
-                                            control={control}
-                                            {...subField}
-                                            label={""}
-                                        />
-                                    </>
-                                ))}
-                            </Box>
-                        </>
-                    );
-                }
-            })}
+            {fields.map((field: FieldProps) => (
+                <Box
+                    sx={{
+                        py: 1,
+                        my: 1,
+                    }}>
+                    {renderField(field)}
+                </Box>
+            ))}
         </Box>
     );
 };
