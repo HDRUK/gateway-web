d06ff958dd5d81f2c58d251317a933ad0cd671f3
Phil Reeks <phil.reeks@hdruk.ac.uk>
2024-01-25 09:49:27 +0000

    wip

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { hotjar } from "react-hotjar";
 import { AppBar, IconButton, Toolbar } from "@mui/material";
+import { unstable_noStore as noStore } from "next/cache";
 import Image from "next/image";
 import Link from "@/components/Link";
 import AccountNavWrapper from "@/modules/AccountNavWrapper";
@@ -12,13 +13,18 @@ import * as styles from "./Header.styles";
 
 /** @jsxImportSource @emotion/react */
 
-const HOTJAR_ID = process.env.NEXT_PUBLIC_HOTJAR_ID;
-const HOTJAR_VERSION = 6;
-
-console.log(
-    `DEBUG ENV TEST - HOTJAR_ID=${process.env[HOTJAR_ENV]} - NEXT_PUBLIC_API_V1_URL=${process.env[PUBLIC_ENV]}`
-);
 function Header() {
+    noStore();
+
+    const HOTJAR_ENV = "NEXT_PUBLIC_HOTJAR_ID";
+    const PUBLIC_ENV = "NEXT_PUBLIC_API_V1_URL";
+    const HOTJAR_ID = process.env[HOTJAR_ENV];
+    const HOTJAR_VERSION = 6;
+
+    console.log(
+        `DEBUG ENV TEST - HOTJAR_ID=${HOTJAR_ID} - NEXT_PUBLIC_API_V1_URL=${process.env[PUBLIC_ENV]}`
+    );
+
     const [showNavList, setShowNavList] = useState(false);
 
     const menuClickHandler = () => {
