86c36f3b8c83ea5491a37241f6f1389a4dde326e
kymmeh <kymmehayley@gmail.com>
2024-01-31 16:38:25 +0000

    Adding test

--- src/utils/general.test.tsx
+++ src/utils/general.test.tsx
@@ -1,4 +1,9 @@
-import { capitalise, splitCamelcase, getTrimmedpathname } from "./general";
+import {
+    capitalise,
+    splitCamelcase,
+    getTrimmedpathname,
+    extractSubdomain,
+} from "./general";
 
 describe("General utils", () => {
     it("should return capitalised string", async () => {
@@ -9,5 +14,10 @@ describe("General utils", () => {
     });
     it("should return string without locale", async () => {
         expect(getTrimmedpathname("en", "/en/the/route")).toEqual("/the/route");
+    });
+    it("should return url without subdomain", async () => {
+        expect(
+            extractSubdomain("https://api.dev.hdruk.cloud.uk/api/va")
+        ).toEqual(".dev.hdruk.cloud.uk");
     });
 });
