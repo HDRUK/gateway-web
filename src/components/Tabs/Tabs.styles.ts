0c3db73ac609e341b1b66b96e6067b397af37012
Phil Reeks <phil.reeks@hdruk.ac.uk>
2024-02-08 14:33:27 +0000

    Style tweak

--- src/components/Tabs/Tabs.styles.ts
+++ src/components/Tabs/Tabs.styles.ts
@@ -25,6 +25,7 @@ export const tabsStyle = {
                 flex: 1,
                 borderRadius: "20px 20px 0px 0px",
                 fontSize: "20px",
+                marginTop: "2px",
                 boxShadow: "1px -1px 3px 0px #2626261A",
                 backgroundColor: colors.white,
                 maxWidth: '220px',
