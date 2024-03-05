350e63bd6d4be6488346d32d3e1cde396589f1f6
kymmeh <kymmehayley@gmail.com>
2024-03-05 15:16:26 +0000

    Fixing test

--- src/components/InfoHoverPanelProps/InfoHoverPanel.test.tsx
+++ src/components/InfoHoverPanelProps/InfoHoverPanel.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import userEvent from "@testing-library/user-event";
-import { render, screen } from "@/utils/testUtils";
+import { render, screen, waitFor } from "@/utils/testUtils";
 import InfoHoverPanel from "./InfoHoverPanel";
 
 describe("InfoHoverPanel", () => {
@@ -19,17 +19,29 @@ describe("InfoHoverPanel", () => {
 
     it("should render default", async () => {
         render(<InfoHoverPanel items={items} />);
-        expect(screen.getByText("Datasets/Tissues")).toBeInTheDocument();
+        expect(screen.getByText("Datasets / Tissues")).toBeInTheDocument();
         expect(
             screen.getByText("Data Uses / Research projects")
         ).toBeInTheDocument();
         expect(screen.getByText("Welcome to The Gateway")).toBeInTheDocument();
+        expect(
+            screen.getByText(
+                "A knowledge graph to explore and access the wealth of health data and research from across the UK and beyond"
+            )
+        ).toBeInTheDocument();
     });
     it("should change on hover default", async () => {
         render(<InfoHoverPanel items={items} />);
 
         const dur = screen.getByText("Data Uses / Research projects");
         userEvent.hover(dur);
-        expect(screen.getByText("Cras ut nisi interdum")).toBeInTheDocument();
+
+        await waitFor(() => {
+            expect(
+                screen.getByText(
+                    "Find data uses/research projects: Search and filter data uses/research projects related to specific datasets and other features"
+                )
+            ).toBeInTheDocument();
+        });
     });
 });
