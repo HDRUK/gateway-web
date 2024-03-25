773f1b19a98027a7ca39c1e191f48e00320ae154
kymmeh <kymmehayley@gmail.com>
2024-03-25 11:06:18 +0000

    Adding link

--- src/config/hrefs.ts
+++ src/config/hrefs.ts
@@ -1,8 +1,11 @@
 const CUSTOMER_PORTAL_SUPPORT_URL = `${process.env.NEXT_PUBLIC_SERVICE_DESK_URL}/customer/portal/1/group/1/create/1`;
 const CUSTOMER_PORTAL_RAISE_SUPPORT_URL = `${process.env.NEXT_PUBLIC_SERVICE_DESK_URL}/customer/portal/1/group/-1`;
 const GATEWAY_TERMS_URL = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/terms-and-conditions`;
+const INTEGRATION_TESTING_URL =
+    "https://hdruk.github.io/gateway-2-integrations-testing/";
 
 export {
+    INTEGRATION_TESTING_URL,
     CUSTOMER_PORTAL_SUPPORT_URL,
     GATEWAY_TERMS_URL,
     CUSTOMER_PORTAL_RAISE_SUPPORT_URL,
