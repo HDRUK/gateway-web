146dadd502ef7eec8b053e761eddfc2b40de18d6
Phil Reeks <phil.reeks@hdruk.ac.uk>
2024-04-11 11:17:33 +0100

    debug

--- src/app/[locale]/(logged-out)/dataset/[datasetId]/page.tsx
+++ src/app/[locale]/(logged-out)/dataset/[datasetId]/page.tsx
@@ -1,16 +1,19 @@
+import Box from "@/components/Box";
+import BoxContainer from "@/components/BoxContainer";
+
 export const metadata = {
     title: "Health Data Research Innovation Gateway - Dataset",
     description: "",
 };
 
-// const DATASET_STAT_PATHS = [
-//     "metadata.metadata.summary.populationSize",
-//     "metadata.metadata.provenance.temporal.startDate",
-//     "metadata.metadata.provenance.temporal.endDate",
-//     "metadata.metadata.coverage.biologicalsamples",
-//     "metadata.metadata.coverage.spatial",
-//     "metadata.metadata.accessibility.access.deliveryLeadTime",
-// ];
+const DATASET_STAT_PATHS = [
+    "metadata.metadata.summary.populationSize",
+    "metadata.metadata.provenance.temporal.startDate",
+    "metadata.metadata.provenance.temporal.endDate",
+    "metadata.metadata.coverage.biologicalsamples",
+    "metadata.metadata.coverage.spatial",
+    "metadata.metadata.accessibility.access.deliveryLeadTime",
+];
 
 export default async function DatasetItemPage({
     params,
@@ -22,7 +25,7 @@ export default async function DatasetItemPage({
     // const cookieStore = cookies();
     // const data = await getDataset(cookieStore, datasetId);
 
-    console.log("datasetId - ", datasetId);
+    console.log("datasetId - ", datasetId, DATASET_STAT_PATHS);
 
     // const datasetVersion = data?.versions?.[0];
     // console.log("datasetVersion - ", datasetVersion);
@@ -39,5 +42,57 @@ export default async function DatasetItemPage({
     //     return { label: section.sectionName };
     // });
 
-    return <p>TEST</p>;
+    return (
+        <BoxContainer
+            sx={{
+                gridTemplateColumns: {
+                    mobile: "repeat(1, 1fr)",
+                    tablet: "repeat(5, 1fr)",
+                },
+            }}>
+            <Box
+                sx={{
+                    gridColumn: { tablet: "span 2", laptop: "span 1" },
+                    bgcolor: "white",
+                    p: 0,
+                }}>
+                {/* <ActiveListSidebar items={activeLinkList} /> */}
+            </Box>
+            <Box
+                sx={{
+                    gridColumn: { tablet: "span 3", laptop: "span 4" },
+                    p: 0,
+                }}>
+                <>
+                    {/* <ActionBar /> */}
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: 2,
+                        }}>
+                        {/* {datasetStats && (
+                            <Box sx={{ p: 0, gap: 2 }}>
+                                <Typography
+                                    variant="h2"
+                                    sx={{ pt: 0.5, pb: 0.5 }}>
+                                    {
+                                        datasetVersion.metadata.metadata.summary
+                                            .title
+                                    }
+                                </Typography>
+                                <div>
+                                    <DatasetStats data={datasetStats} />
+                                </div>
+                            </Box>
+                        )} */}
+                        {/* <DatasetContent
+                            data={datasetVersion}
+                            populatedSections={populatedSections}
+                        /> */}
+                    </Box>
+                </>
+            </Box>
+        </BoxContainer>
+    );
 }
