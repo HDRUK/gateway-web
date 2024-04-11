d504680010dc287100fe7dab3998abaef8b79a0e
Phil Reeks <phil.reeks@hdruk.ac.uk>
2024-04-11 10:50:44 +0100

    debug

--- src/app/[locale]/(logged-out)/dataset/[datasetId]/page.tsx
+++ src/app/[locale]/(logged-out)/dataset/[datasetId]/page.tsx
@@ -1,23 +1,16 @@
-import { get, isEmpty, pick } from "lodash";
-import { cookies } from "next/headers";
-import Box from "@/components/Box";
-import BoxContainer from "@/components/BoxContainer";
-import { getDataset } from "@/utils/api";
-import { datasetFields } from "./config";
-
 export const metadata = {
     title: "Health Data Research Innovation Gateway - Dataset",
     description: "",
 };
 
-const DATASET_STAT_PATHS = [
-    "metadata.metadata.summary.populationSize",
-    "metadata.metadata.provenance.temporal.startDate",
-    "metadata.metadata.provenance.temporal.endDate",
-    "metadata.metadata.coverage.biologicalsamples",
-    "metadata.metadata.coverage.spatial",
-    "metadata.metadata.accessibility.access.deliveryLeadTime",
-];
+// const DATASET_STAT_PATHS = [
+//     "metadata.metadata.summary.populationSize",
+//     "metadata.metadata.provenance.temporal.startDate",
+//     "metadata.metadata.provenance.temporal.endDate",
+//     "metadata.metadata.coverage.biologicalsamples",
+//     "metadata.metadata.coverage.spatial",
+//     "metadata.metadata.accessibility.access.deliveryLeadTime",
+// ];
 
 export default async function DatasetItemPage({
     params,
@@ -26,77 +19,25 @@ export default async function DatasetItemPage({
 }) {
     const { datasetId } = params;
 
-    const cookieStore = cookies();
-    const data = await getDataset(cookieStore, datasetId);
+    // const cookieStore = cookies();
+    // const data = await getDataset(cookieStore, datasetId);
 
-    console.log("data - ", data);
+    console.log("datasetId - ", datasetId);
 
-    const datasetVersion = data?.versions?.[0];
-    console.log("datasetVersion - ", datasetVersion);
+    // const datasetVersion = data?.versions?.[0];
+    // console.log("datasetVersion - ", datasetVersion);
 
-    const datasetStats = pick(datasetVersion, DATASET_STAT_PATHS);
-    console.log("datasetStats - ", datasetStats);
+    // const datasetStats = pick(datasetVersion, DATASET_STAT_PATHS);
+    // console.log("datasetStats - ", datasetStats);
 
-    const populatedSections = datasetFields.filter(section =>
-        section.fields.some(field => !isEmpty(get(datasetVersion, field.path)))
-    );
-    console.log("populatedSections - ", populatedSections);
+    // const populatedSections = datasetFields.filter(section =>
+    //     section.fields.some(field => !isEmpty(get(datasetVersion, field.path)))
+    // );
+    // console.log("populatedSections - ", populatedSections);
 
     // const activeLinkList = populatedSections.map(section => {
     //     return { label: section.sectionName };
     // });
 
-    return (
-        <BoxContainer
-            sx={{
-                gridTemplateColumns: {
-                    mobile: "repeat(1, 1fr)",
-                    tablet: "repeat(5, 1fr)",
-                },
-            }}>
-            <Box
-                sx={{
-                    gridColumn: { tablet: "span 2", laptop: "span 1" },
-                    bgcolor: "white",
-                    p: 0,
-                }}>
-                {/* <ActiveListSidebar items={activeLinkList} /> */}
-            </Box>
-            <Box
-                sx={{
-                    gridColumn: { tablet: "span 3", laptop: "span 4" },
-                    p: 0,
-                }}>
-                <>
-                    {/* <ActionBar /> */}
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: 2,
-                        }}>
-                        {/* {datasetStats && (
-                            <Box sx={{ p: 0, gap: 2 }}>
-                                <Typography
-                                    variant="h2"
-                                    sx={{ pt: 0.5, pb: 0.5 }}>
-                                    {
-                                        datasetVersion.metadata.metadata.summary
-                                            .title
-                                    }
-                                </Typography>
-                                <div>
-                                    <DatasetStats data={datasetStats} />
-                                </div>
-                            </Box>
-                        )} */}
-                        {/* <DatasetContent
-                            data={datasetVersion}
-                            populatedSections={populatedSections}
-                        /> */}
-                    </Box>
-                </>
-            </Box>
-        </BoxContainer>
-    );
+    return <p>TEST</p>;
 }
