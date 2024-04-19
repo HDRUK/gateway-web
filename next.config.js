6badcdc635a2053a21b5e183db8dde39f903942f
Phil Reeks <phil.reeks@hdruk.ac.uk>
2024-04-19 15:42:43 +0100

    enable type check

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const nextConfig = withNextIntl({
         // Dangerously allow production builds to successfully complete even if
         // your project has type errors.
         // !! WARN !!
-        ignoreBuildErrors: true,
+        ignoreBuildErrors: false,
     },
 });
 
