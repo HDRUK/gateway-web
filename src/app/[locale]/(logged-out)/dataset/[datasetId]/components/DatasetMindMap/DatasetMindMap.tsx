f9963e250ccf05652fc7bd67d407d6781125aa6a
Calum Macdonald <calmacx@gmail.com>
2024-04-24 13:48:16 +0100

    change props for datasetmindmap

--- src/app/[locale]/(logged-out)/dataset/[datasetId]/components/DatasetMindMap/DatasetMindMap.tsx
+++ src/app/[locale]/(logged-out)/dataset/[datasetId]/components/DatasetMindMap/DatasetMindMap.tsx
@@ -4,7 +4,7 @@ import { useMemo } from "react";
 import { VersionItem } from "@/interfaces/Dataset";
 import Box from "@/components/Box";
 import BoxContainer from "@/components/BoxContainer";
-import MindMap from "@/components/MindMap";
+import MindMap, { MindMapProps } from "@/components/MindMap/MindMap";
 import Paper from "@/components/Paper";
 import {
     rootNode,
@@ -13,7 +13,19 @@ import {
     connectionLineStyle,
 } from "@/config/mindmaps/dataset";
 
-const DatasetMindMap = ({ data }: { data: VersionItem }) => {
+interface DatasetMindMapProps extends MindMapProps {
+    data: VersionItem;
+}
+
+const DatasetMindMap = ({
+    data,
+    panOnDrag = false,
+    panOnScroll = false,
+    zoomOnScroll = false,
+    zoomOnPinch = false,
+    zoomOnDoubleClick = false,
+    ...rest
+}: DatasetMindMapProps) => {
     const hydratedOuterNodes = useMemo(
         () =>
             outerNodes.map(node => {
@@ -59,15 +71,16 @@ const DatasetMindMap = ({ data }: { data: VersionItem }) => {
                 }}>
                 <Paper sx={{ borderRadius: 2, p: 2, height: "350px" }}>
                     <MindMap
+                        panOnDrag={panOnDrag}
+                        panOnScroll={panOnScroll}
+                        zoomOnScroll={zoomOnScroll}
+                        zoomOnPinch={zoomOnPinch}
+                        zoomOnDoubleClick={zoomOnDoubleClick}
+                        {...rest}
                         rootNode={rootNode}
                         outerNodes={hydratedOuterNodes}
                         initialEdges={initialEdges}
                         connectionLineStyle={connectionLineStyle}
-                        panOnDrag={false}
-                        panOnScroll={false}
-                        zoomOnScroll={false}
-                        zoomOnPinch={false}
-                        zoomOnDoubleClick={false}
                     />
                 </Paper>
             </Box>
