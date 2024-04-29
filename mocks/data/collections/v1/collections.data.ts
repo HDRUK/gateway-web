2647c27c0b20e531611c1d8e782092e9c9096197
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-04-29 09:56:56 +0100

    chore: change mock

--- mocks/data/collections/v1/collections.data.ts
+++ mocks/data/collections/v1/collections.data.ts
@@ -3,20 +3,7 @@ import { Collection } from "@/interfaces/Collection";
 
 const generateCollectionV1 = (data = {}): Collection => {
     return {
-        _id: faker.datatype.uuid(),
-        _explanation: [faker.datatype.string(), faker.datatype.string()],
-        _score: faker.datatype.number(),
-        _source: {
-            name: faker.datatype.string(),
-            datasetAbstracts: [
-                faker.datatype.string(),
-                faker.datatype.string(),
-            ],
-            datasetTitles: [faker.datatype.string(), faker.datatype.string()],
-            description: faker.datatype.string(),
-            keywords: [faker.word.noun()],
-            publisherName: faker.company.name(),
-        },
+        name: faker.datatype.string(),
         ...data,
     };
 };
