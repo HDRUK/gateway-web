32d96cecc1a380668d942a522bb1fb896395759e
Calum Macdonald <calmacx@gmail.com>
2024-05-09 12:30:39 +0100

    add testing

---
+++ src/app/[locale]/account/profile/dar-admin/question-bank/list/QuestionBankListItem/QuestionBankListItem.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@/utils/testUtils";
+import { generateQuestionBankQuestion } from "@/mocks/data/questionBank";
+import QuestionBankListItem from "./QuestionBankListItem";
+
+describe("Question Bank Question Card", () => {
+    const mockResult = generateQuestionBankQuestion();
+    it("should render the title and guidance from the question JSON", async () => {
+        render(<QuestionBankListItem data={mockResult} />);
+
+        expect(
+            screen.getByText(mockResult.question_json.title)
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByText(mockResult.question_json.guidance)
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByText(mockResult.locked ? "Locked" : "Available")
+        ).toBeInTheDocument();
+    });
+});
