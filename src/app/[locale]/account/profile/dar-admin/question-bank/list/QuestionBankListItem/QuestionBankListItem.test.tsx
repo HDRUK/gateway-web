4c85944a486ce54c20bc5f05abf4f9c7c7fdb74f
Calum Macdonald <calmacx@gmail.com>
2024-05-09 14:38:50 +0100

    fixing the testing?

--- src/app/[locale]/account/profile/dar-admin/question-bank/list/QuestionBankListItem/QuestionBankListItem.test.tsx
+++ src/app/[locale]/account/profile/dar-admin/question-bank/list/QuestionBankListItem/QuestionBankListItem.test.tsx
@@ -5,7 +5,7 @@ import QuestionBankListItem from "./QuestionBankListItem";
 describe("Question Bank Question Card", () => {
     const mockResult = generateQuestionBankQuestion();
     it("should render the title and guidance from the question JSON", async () => {
-        render(<QuestionBankListItem data={mockResult} />);
+        render(<QuestionBankListItem data={mockResult} actions={[]} />);
 
         expect(
             screen.getByText(mockResult.question_json.title)
