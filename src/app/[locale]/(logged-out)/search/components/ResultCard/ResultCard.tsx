cfe8752154486939e2d8c687719b3128011a0041
Sam Cox <sam.cox@nottingham.ac.uk>
2024-05-16 14:04:12 +0100

    Display highlights on Dataset results cards, with yellow background highlighting. Includes styled Highlight component.

--- src/app/[locale]/(logged-out)/search/components/ResultCard/ResultCard.tsx
+++ src/app/[locale]/(logged-out)/search/components/ResultCard/ResultCard.tsx
@@ -8,7 +8,7 @@ import Box from "@/components/Box";
 import Typography from "@/components/Typography";
 import { RouteName } from "@/consts/routeName";
 import { getDateRange, getPopulationSize } from "@/utils/search";
-
+import { Highlight } from "./ResultCard.styles";
 interface ResultCardProps {
     result: SearchResultDataset;
 }
@@ -19,6 +19,7 @@ const ResultCard = ({ result }: ResultCardProps) => {
     const t = useTranslations(TRANSLATION_PATH);
     const router = useRouter();
     const metadata = get(result, "metadata");
+    const highlight = get(result, "highlight");
     const { _id: datasetId } = result;
 
     const handleClickItem = useCallback(() => {
@@ -26,6 +27,15 @@ const ResultCard = ({ result }: ResultCardProps) => {
     }, [datasetId, router]);
 
     if (!metadata) return null;
+
+    // If available, display the first of the highlights from the abstract, or failing that from the description. 
+    // Fallback is the (un-highlighted) abstract.
+    const text_to_display =
+        highlight?.abstract !== undefined
+            ? highlight.abstract[0]
+            : highlight?.description !== undefined
+            ? highlight.description[0]
+            : metadata.summary.abstract;
 
     return (
         <>
@@ -55,13 +65,13 @@ const ResultCard = ({ result }: ResultCardProps) => {
                                         : metadata.summary.publisher
                                               .publisherName}
                                 </Typography>
-                                <Typography
+                                <Highlight
                                     sx={{ mb: 1.5 }}
                                     component="div"
                                     variant="body2"
-                                    color="text.gray">
-                                    {metadata.summary.abstract}
-                                </Typography>
+                                    color="text.gray"
+                                    dangerouslySetInnerHTML={{ __html: text_to_display}}>
+                                </Highlight>
                                 <Box
                                     sx={{
                                         p: 0,
