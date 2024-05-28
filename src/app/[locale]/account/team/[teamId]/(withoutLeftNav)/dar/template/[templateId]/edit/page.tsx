413c18a132b8ba775ec17cefeefe0a51fa79b20b
Calum Macdonald <calmacx@gmail.com>
2024-05-28 13:11:24 +0100

    cleaning up

--- src/app/[locale]/account/team/[teamId]/(withoutLeftNav)/dar/template/[templateId]/edit/page.tsx
+++ src/app/[locale]/account/team/[teamId]/(withoutLeftNav)/dar/template/[templateId]/edit/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 
-import { useParams } from "next/navigation";
 import BackButton from "@/components/BackButton";
 import EditTemplate from "../components/EditTemplate";
 
-const EditTemplatePage = () => {
-    const { teamId, templateId } = useParams<{
-        teamId: string;
-        templateId: string;
-    }>();
-
+const EditTemplatePage = ({ params }: { params: { templateId: string } }) => {
     return (
         <>
             <BackButton label="Back to DAR admin" />
-            <EditTemplate templateId={templateId} />
+            <EditTemplate templateId={params.templateId} />
         </>
     );
 };
