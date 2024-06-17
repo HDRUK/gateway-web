c7aac561b838028330193d277010236506be8990
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-06-17 12:59:27 +0100

    FEATURE

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -1,26 +1,52 @@
-import styled from "@emotion/styled";
+import { styled } from "@mui/material";
 import Container from "@/components/Container";
 import Link from "@/components/Link";
 
 export const FooterWrapper = styled("div")({
     fontSize: "14px",
     backgroundColor: "#29235c",
+    ul: {
+        listStyleType: "none",
+        paddingLeft: 0,
+    },
 });
 
-export const FooterContainer = styled(Container)({
+export const FooterContainer = styled(Container)(({ theme }) => ({
     color: "white",
     padding: "50px 20px",
-});
+    display: "flex",
+    gap: "200px",
+    [theme.breakpoints.down("tablet")]: {
+        flexDirection: "column",
+        gap: "16px",
+    },
+}));
 
-export const FooterList = styled("ul")({
-    listStyle: "none",
+export const FooterSocialLinks = styled("ul")(({ theme }) => ({
     textDecoration: "none",
     gap: "20px",
     paddingLeft: 0,
-    "@media (min-width: 420px)": {
-        display: "flex",
+    display: "flex",
+    [theme.breakpoints.down("tablet")]: {
+        flexDirection: "column",
+        gap: "16px",
     },
+}));
+
+export const FooterLinks = styled("ul")({
+    display: "flex",
+    flexDirection: "column",
+    gap: "12px",
 });
+
+export const FooterLinksContainer = styled("ul")(({ theme }) => ({
+    display: "flex",
+    gap: "40px",
+    [theme.breakpoints.down("tablet")]: {
+        flexDirection: "column",
+        gap: "16px",
+    },
+}));
 
 export const FooterSocial = styled("span")({
     display: "flex",
