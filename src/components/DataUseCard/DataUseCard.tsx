61ea6acf6c7c56139051bf33ebabcb5b3d49bc25
Sam Cox <sam.cox@nottingham.ac.uk>
2024-07-05 15:24:47 +0100

    Fix dataset link and tidy up code.

--- src/components/DataUseCard/DataUseCard.tsx
+++ src/components/DataUseCard/DataUseCard.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@mui/material";
-import { get } from "lodash";
+import { useParams } from "next/navigation";
 import { DataUse } from "@/interfaces/DataUse";
 import { IconType } from "@/interfaces/Ui";
 import Box from "@/components/Box";
@@ -7,10 +7,9 @@ import KeyValueList from "@/components/KeyValueList";
 import Paper from "@/components/Paper";
 import Typography from "@/components/Typography";
 import { colors } from "@/config/theme";
-import { getLatestVersion } from "@/utils/dataset";
+import { RouteName } from "@/consts/routeName";
 import { formatDate } from "@/utils/date";
 import CardActions from "../CardActions";
-import { RouteName } from "@/consts/routeName";
 
 interface DataUseCardProps {
     dataUse: DataUse;
@@ -23,56 +22,24 @@ interface DataUseCardProps {
 }
 
 const DataUseCard = ({ dataUse, actions }: DataUseCardProps) => {
-    // const latestMetadata = getLatestVersion(dataUse);
-
-    // if (!latestMetadata) return null;
-
-    // const {
-    //     version,
-    //     updated_at,
-    //     metadata: { metadata, gwdmVersion },
-    // } = latestMetadata;
-
+    const params = useParams<{ locale: string }>();
     const title = dataUse.project_title;
     const providerName = dataUse.team.name;
     const applicantName = dataUse.user.name;
 
-    console.log(dataUse);
-    // console.log(dataUse["datasets"]);
     let datasetNames: JSX.Element[] = [];
     dataUse.datasets.forEach((dataset, idx) => {
         // don't add a comma to the last element
         datasetNames.push(
-            <span>
-                <Link href="https://example.com">{dataset.shortTitle}</Link>
+            <>
+                <Link
+                    href={`/${params?.locale}/${RouteName.DATASET_ITEM}/${dataset.id}`}>
+                    {dataset.shortTitle}
+                </Link>
                 {idx + 1 < dataUse.datasets.length && ", "}
-            </span>
+            </>
         );
     });
-
-    console.log(datasetNames);
-    // let datasetLinks: JSX.Element = (
-    //     <Box>{datasetNames.map((datasetLink, idx) =>
-    //         [idx > 0 && ", ",
-    //         datasetLink]
-    //         )}</Box>
-    // );
-    // console.log(datasetLinks);
-    // console.log(datasetNames);
-
-    // const datasets = dataUse["datasets"]; //
-    // const publisherName = get(
-    //     metadata,
-    //     gwdmVersion === undefined || gwdmVersion === "1.0"
-    //         ? "summary.publisher.publisherName"
-    //         : "summary.publisher.name"
-    // ) as unknown as string;
-
-    // const originMapping = {
-    //     MANUAL: "Manually",
-    //     API: "API",
-    //     FMA: "FMA",
-    // };
 
     return (
         <Paper data-testid="datause-card" sx={{ mb: 2 }}>
