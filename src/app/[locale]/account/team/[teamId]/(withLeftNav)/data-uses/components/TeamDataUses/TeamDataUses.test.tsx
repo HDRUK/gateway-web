04d5bb8c3009977299e73daa8a6210ae91a186c8
Sam Cox <sam.cox@nottingham.ac.uk>
2024-07-08 11:12:18 +0100

    Add testing.

--- src/app/[locale]/account/team/[teamId]/(withLeftNav)/data-uses/components/TeamDataUses/TeamDataUses.test.tsx
+++ src/app/[locale]/account/team/[teamId]/(withLeftNav)/data-uses/components/TeamDataUses/TeamDataUses.test.tsx
@@ -1,112 +1,59 @@
 import mockRouter from "next-router-mock";
 import { render, screen, waitFor, within } from "@/utils/testUtils";
-import { generateDatasetV1 } from "@/mocks/data/dataset";
-import { getDatasetsV1 } from "@/mocks/handlers/datasets";
+import { generateDataUse } from "@/mocks/data/dataUse";
+import { getDataUses } from "@/mocks/handlers/dataUses";
 import { server } from "@/mocks/server";
-import TeamDatasets from "./TeamDataUses";
+import TeamDataUses from "./TeamDataUses";
 
 mockRouter.query = { teamId: "1", tab: "ACTIVE" };
 window.scrollTo = jest.fn();
 
-describe("TeamDatasets", () => {
-    it("should render all datasets (filtered on BE)", async () => {
-        const mockDatasets = [
-            generateDatasetV1("1.0", {
-                create_origin: "MANUAL",
+describe("TeamDataUses", () => {
+    it("should render all data uses (filtered on BE)", async () => {
+        const mockDataUses = [
+            generateDataUse({
                 status: "ARCHIVED",
             }),
-            generateDatasetV1("1.0", {
-                create_origin: "API",
+            generateDataUse({
                 status: "ACTIVE",
             }),
-            generateDatasetV1("1.0", { create_origin: "FMA", status: "DRAFT" }),
+            generateDataUse({ status: "DRAFT" }),
         ];
-        server.use(getDatasetsV1(mockDatasets));
-        render(<TeamDatasets permissions={{}} />);
+        server.use(getDataUses(mockDataUses));
+        render(<TeamDataUses permissions={{}} />);
 
         await waitFor(() => {
-            const datasetCards = screen.getAllByTestId("dataset-card");
-            expect(datasetCards).toHaveLength(3);
+            const dataUseCards = screen.getAllByTestId("datause-card");
+            expect(dataUseCards).toHaveLength(3);
 
             expect(
-                within(datasetCards[0]).getByText(
-                    `${mockDatasets[0].versions[0].metadata.metadata.summary.title}`
+                within(dataUseCards[0]).getByText(
+                    `${mockDataUses[0].project_title}`
                 )
             ).toBeInTheDocument();
             expect(
-                within(datasetCards[0]).getByText(
-                    `${mockDatasets[0].versions[0].metadata.metadata.summary.publisher.publisherName}`
+                within(dataUseCards[0]).getByText(
+                    `${mockDataUses[0].team.name}`
                 )
             ).toBeInTheDocument();
             expect(
-                within(datasetCards[0]).getByText(
-                    `${mockDatasets[0].versions[0].version}`
+                within(dataUseCards[0]).getByText(
+                    `${mockDataUses[0].datasets[0].shortTitle}`
                 )
-            ).toBeInTheDocument();
-
-            expect(
-                within(datasetCards[0]).getByText(`Manually created dataset`)
-            ).toBeInTheDocument();
-            expect(
-                within(datasetCards[1]).getByText(`API created dataset`)
-            ).toBeInTheDocument();
-            expect(
-                within(datasetCards[2]).getByText(`FMA created dataset`)
             ).toBeInTheDocument();
         });
     });
     it("should render message if no active datasets", async () => {
-        server.use(getDatasetsV1([]));
-        render(<TeamDatasets permissions={{}} />);
+        server.use(getDataUses([]));
+        render(<TeamDataUses permissions={{}} />);
 
         await waitFor(() => {
             expect(
                 screen.getByText(
-                    "No active datasets found on the Gateway for your team."
+                    "No active data uses found on the Gateway for your team."
                 )
             ).toBeInTheDocument();
         });
     });
 
-    it("should render all datasets (with different GWDM versions)", async () => {
-        const mockDatasets = [
-            generateDatasetV1("1.0", {
-                create_origin: "API",
-                status: "ACTIVE",
-            }),
-            generateDatasetV1("1.0", {
-                create_origin: "API",
-                status: "ACTIVE",
-            }),
-            generateDatasetV1("1.1", {
-                create_origin: "API",
-                status: "ACTIVE",
-            }),
-        ];
-        server.use(getDatasetsV1(mockDatasets));
-        render(<TeamDatasets permissions={{}} />);
-
-        await waitFor(() => {
-            const datasetCards = screen.getAllByTestId("dataset-card");
-            expect(datasetCards).toHaveLength(3);
-
-            expect(
-                within(datasetCards[0]).getByText(
-                    `${mockDatasets[0].versions[0].metadata.metadata.summary.publisher.publisherName}`
-                )
-            ).toBeInTheDocument();
-
-            expect(
-                within(datasetCards[1]).getByText(
-                    `${mockDatasets[1].versions[0].metadata.metadata.summary.publisher.publisherName}`
-                )
-            ).toBeInTheDocument();
-
-            expect(
-                within(datasetCards[2]).getByText(
-                    `${mockDatasets[2].versions[0].metadata.metadata.summary.publisher.name}`
-                )
-            ).toBeInTheDocument();
-        });
-    });
 });
