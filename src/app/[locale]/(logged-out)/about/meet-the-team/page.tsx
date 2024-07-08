5b6ceb733b2414f9c04c9dae4ab05389ff2c00d9
Phil Reeks <phil.reeks@hdruk.ac.uk>
2024-07-08 10:01:41 +0100

    debug

--- src/app/[locale]/(logged-out)/about/meet-the-team/page.tsx
+++ src/app/[locale]/(logged-out)/about/meet-the-team/page.tsx
@@ -1,13 +1,12 @@
-import { getMeetTheTeam } from "@/utils/cms";
-import TeamMembers from "./components/TeamMembers";
-
 export const metadata = {
     title: "Health Data Research Innovation Gateway - About - Meet the team",
     description: "",
 };
 
 export default async function MeetTheTeam() {
-    const data = await getMeetTheTeam();
+    // const data = await getMeetTheTeam();
 
-    return <TeamMembers data={data[0]} />;
+    // return <TeamMembers data={data[0]} />;
+
+    return <p>TEST</p>;
 }
