f063151dc70a9ca15ad95166029013b48294a4e5
Sam Cox <sam.cox@nottingham.ac.uk>
2024-08-13 09:45:20 +0100

    Just saving stuff to come back to.

--- src/components/MenuDropdown/MenuDropdown.tsx
+++ src/components/MenuDropdown/MenuDropdown.tsx
@@ -1,7 +1,9 @@
 import { Menu, MenuItem } from "@mui/material";
 import Button from "@/components/Button";
 import Link from "@/components/Link";
+import useDialog from "@/hooks/useDialog";
 import { colors } from "@/config/theme";
+import { dialogPropsType } from "@/providers/DialogProvider";
 
 interface MenuDropdownProps {
     title?: string;
@@ -13,6 +15,7 @@ interface MenuDropdownProps {
         subItems?: { label: string; href: string }[];
         divider?: boolean;
         icon?: HTMLElement;
+        dialog?;
     }[];
     handleClose: () => void;
     transformOrigin?: null | {
@@ -33,6 +36,12 @@ function MenuDropdown({
     anchorOrigin,
     title,
 }: MenuDropdownProps) {
+    const { showDialog } = useDialog();
+
+    const handleShowDialog = dialog => {
+        showDialog(dialog);
+    };
+
     return (
         <Menu
             anchorEl={anchorElement}
@@ -88,6 +97,19 @@ function MenuDropdown({
                                 onClick={menuItem.action}
                                 variant="link"
                                 aria-label={ariaLabel}
+                                sx={{ pl: 0 }}>
+                                {menuItem.label}
+                            </Button>
+                        </MenuItem>
+                    );
+                }
+                if (menuItem.dialog) {
+                    return (
+                        <MenuItem key={menuItem.label} sx={{ maxWidth: 250 }}>
+                            {menuItem.icon || null}
+                            <Button
+                                onClick={() => handleShowDialog(menuItem.dialog)}
+                                variant="link"
                                 sx={{ pl: 0 }}>
                                 {menuItem.label}
                             </Button>
