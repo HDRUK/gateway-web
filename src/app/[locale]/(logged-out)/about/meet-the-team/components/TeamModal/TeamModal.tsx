dac206fb2659dbf6bb36cef7c86de35415df09a9
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2024-08-20 14:08:06 +0100

    remove unused usecallback

--- src/app/[locale]/(logged-out)/about/meet-the-team/components/TeamModal/TeamModal.tsx
+++ src/app/[locale]/(logged-out)/about/meet-the-team/components/TeamModal/TeamModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { useTranslations } from "next-intl";
 import { TeamMember } from "@/interfaces/MeetTheTeam";
@@ -16,48 +16,47 @@ const TRANSLATIONS_NAMESPACE_TEAM_MODAL = "components.TeamModal";
 export default function TeamModal({ onCancel, teamMember }: TeamModalProps) {
     const { showModal } = useModal();
     const t = useTranslations(TRANSLATIONS_NAMESPACE_TEAM_MODAL);
-    const modalContent = useCallback(
-        () => (
-            <Box sx={{ display: "flex", gap: 2 }}>
-                <div>
-                    <Box
-                        component="img"
-                        src={teamMember.image.node.sourceUrl}
-                        alt={teamMember.image.node.altText}
-                        sx={{ width: "200px" }}
-                    />
-                </div>
-                <div>
-                    <Typography variant="h3" sx={{ mb: 2 }}>
-                        {teamMember.name}
-                    </Typography>
-                    <Typography variant="h4" sx={{ mb: 2 }}>
-                        {teamMember.jobTitle}
-                    </Typography>
-                    <Typography variant="subtitle2">
-                        {t("descriptionTitle")}
-                    </Typography>
-                    <Typography>{teamMember.info}</Typography>
-                </div>
-            </Box>
-        ),
-        [
-            teamMember.image.node.sourceUrl,
-            teamMember.image.node.altText,
-            teamMember.info,
-            teamMember.name,
-            teamMember.jobTitle,
-            t,
-        ]
-    );
     useEffect(() => {
         showModal({
-            content: modalContent(),
+            content: (
+                <Box sx={{ display: "flex", gap: 2 }}>
+                    <div>
+                        <Box
+                            component="img"
+                            src={teamMember.image.node.sourceUrl}
+                            alt={teamMember.image.node.altText}
+                            sx={{ width: "200px" }}
+                        />
+                    </div>
+                    <div>
+                        <Typography variant="h3" sx={{ mb: 2 }}>
+                            {teamMember.name}
+                        </Typography>
+                        <Typography variant="h4" sx={{ mb: 2 }}>
+                            {teamMember.jobTitle}
+                        </Typography>
+                        <Typography variant="subtitle2">
+                            {t("descriptionTitle")}
+                        </Typography>
+                        <Typography>{teamMember.info}</Typography>
+                    </div>
+                </Box>
+            ),
             showCancel: false,
             showConfirm: false,
             onCancel,
         });
-    }, [onCancel, showModal, t, modalContent, teamMember]);
+    }, [
+        onCancel,
+        showModal,
+        t,
+        teamMember,
+        teamMember.image.node.sourceUrl,
+        teamMember.image.node.altText,
+        teamMember.info,
+        teamMember.name,
+        teamMember.jobTitle,
+    ]);
 
     return null;
 }
