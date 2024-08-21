fc9941e34a20f0a7f58908baab4155604715c346
Sam Cox <sam.cox@nottingham.ac.uk>
2024-08-21 22:16:27 +0100

    Lint, and use preferred email.

--- src/app/[locale]/(logged-out)/dataset/[datasetId]/components/GeneralEnquirySidebar/GeneralEnquirySidebar.tsx
+++ src/app/[locale]/(logged-out)/dataset/[datasetId]/components/GeneralEnquirySidebar/GeneralEnquirySidebar.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { toNumber } from "lodash";
 import { useEffect } from "react";
-import { useTranslations } from "next-intl";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Typography } from "@mui/material";
-import useSidebar from "@/hooks/useSidebar";
+import { toNumber } from "lodash";
+import { useTranslations } from "next-intl";
+import { useParams } from "next/navigation";
+import { Enquiry } from "@/interfaces/Enquiry";
+import { User } from "@/interfaces/User";
 import Box from "@/components/Box";
 import BoxContainer from "@/components/BoxContainer";
 import Button from "@/components/Button";
@@ -12,20 +16,16 @@ import Form from "@/components/Form";
 import InputWrapper from "@/components/InputWrapper";
 import useAuth from "@/hooks/useAuth";
 import usePost from "@/hooks/usePost";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Enquiry } from "@/interfaces/Enquiry";
+import useSidebar from "@/hooks/useSidebar";
+import apis from "@/config/apis";
 import {
     generalEnquiryFormFields,
     generalEnquiryValidationSchema,
     generalEnquiryDefaultValues,
 } from "@/config/forms/generalEnquiry";
-import apis from "@/config/apis";
-import { User } from "@/interfaces/User";
-import { useParams } from "next/navigation";
+import { getPreferredEmail } from "@/utils/user";
 
 const TRANSLATION_PATH = "pages.dataset.components.DatasetStats";
-
 
 const GeneralEnquirySidebar = ({
     teamId,
@@ -62,10 +62,10 @@ const GeneralEnquirySidebar = ({
     const hydratedFormFields = generalEnquiryFormFields;
 
     const submitForm = async (formData: Enquiry) => {
-        console.log("SUBMIT GENERAL ENQUIRY", formData);
         if (!user) return;
+
         const minUser = { id: user.id };
-        console.log(user);
+
         const payload = {
             ...minUser,
             ...formData,
@@ -79,25 +79,18 @@ const GeneralEnquirySidebar = ({
                     interest_type: "PRIMARY",
                 },
             ],
-            // dataset_parts_known: "",
-            // enabled: true,
-            from: user.email, //TODO: use logic to use secondary if preferred
+            from: getPreferredEmail(user),
             is_dar_dialogue: false,
             is_dar_status: false,
-            // other_datasets: "",
-            // unique_key: "78634786478",
             is_feasibility_enquiry: false,
             is_general_enquiry: true,
-            // query: "",
         };
-        console.log("payload");
-        console.log(payload);
+
         await sendEnquiry(payload).then(res => {
             if (res) {
                 hideSidebar();
-            };
+            }
         });
-
     };
 
     useEffect(() => {
@@ -108,76 +101,58 @@ const GeneralEnquirySidebar = ({
     }, [reset, user]);
 
     return (
-                <>
-                    <BoxContainer
-                        sx={{
-                            gridTemplateColumns: {
-                                tablet: "repeat(4, 1fr)",
-                            },
-                            gap: {
-                                mobile: 1,
-                                tablet: 2,
-                            },
-                            p: 0,
-                        }}>
+        <>
+            <BoxContainer
+                sx={{
+                    gridTemplateColumns: {
+                        tablet: "repeat(4, 1fr)",
+                    },
+                    gap: {
+                        mobile: 1,
+                        tablet: 2,
+                    },
+                    p: 0,
+                }}>
+                <Box
+                    sx={{
+                        gridColumn: {
+                            tablet: "span 4",
+                            laptop: "span 4",
+                        },
+                    }}>
+                    <Typography variant="h1">
+                        {teamMemberOf} {">"} {teamName}
+                    </Typography>
+                    <Typography>
+                        Send a general enquiry to one or multiple Data
+                        Custodians. You will receive an email copy of the
+                        enquiry sent. The Data Custodian(s) will reply via email
+                        to your preferred email address, with a copy shared with
+                        the Gateway.
+                    </Typography>
+
+                    <Form sx={{ mt: 3 }} onSubmit={handleSubmit(submitForm)}>
+                        {hydratedFormFields.map(field => (
+                            <InputWrapper
+                                key={field.name}
+                                control={control}
+                                {...field}
+                            />
+                        ))}
+
                         <Box
                             sx={{
-                                gridColumn: {
-                                    tablet: "span 4",
-                                    laptop: "span 4",
-                                },
+                                p: 0,
+                                display: "flex",
+                                justifyContent: "end",
                             }}>
-                            <Typography variant="h1">
-                                {teamMemberOf} {">"}{" "}
-                                {teamName}
-                            </Typography>
-                            <Typography>
-                                Send a general enquiry to
-                                one or multiple Data
-                                Custodians. You will receive
-                                an email copy of the enquiry
-                                sent. The Data Custodian(s)
-                                will reply via email to your
-                                preferred email address,
-                                with a copy shared with the
-                                Gateway.
-                            </Typography>
-
-                            <Form
-                                sx={{ mt: 3 }}
-                                onSubmit={handleSubmit(
-                                    submitForm
-                                )}>
-                                {hydratedFormFields.map(
-                                    field => (
-                                        <InputWrapper
-                                            key={field.name}
-                                            control={
-                                                control
-                                            }
-                                            {...field}
-                                        />
-                                    )
-                                )}
-
-                                <Box
-                                    sx={{
-                                        p: 0,
-                                        display: "flex",
-                                        justifyContent:
-                                            "end",
-                                    }}>
-                                    <Button type="submit">
-                                        Save changes
-                                    </Button>
-                                </Box>
-                            </Form>
+                            <Button type="submit">Save changes</Button>
                         </Box>
-                    </BoxContainer>
-                </>
-            )
-        ;
-
+                    </Form>
+                </Box>
+            </BoxContainer>
+        </>
+    );
 };
 
 export default GeneralEnquirySidebar;
