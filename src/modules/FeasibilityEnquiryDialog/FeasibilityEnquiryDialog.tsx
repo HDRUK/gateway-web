8e3915466067d8b84fec5708da6a808cf1f3d3eb
Sam Cox <sam.cox@nottingham.ac.uk>
2024-08-30 15:33:15 +0100

    Pass dataset detail to feas form from table view.

--- src/modules/FeasibilityEnquiryDialog/FeasibilityEnquiryDialog.tsx
+++ src/modules/FeasibilityEnquiryDialog/FeasibilityEnquiryDialog.tsx
@@ -4,13 +4,11 @@ import { useTranslations } from "next-intl";
 import { SearchResultDataset } from "@/interfaces/Search";
 import Box from "@/components/Box";
 import Dialog from "@/components/Dialog";
-import EllipsisCharacterLimit from "@/components/EllipsisCharacterLimit";
 import Typography from "@/components/Typography";
 import useDialog from "@/hooks/useDialog";
-import FeasibilityEnquirySidebar from "@/app/[locale]/(logged-out)/search/components/FeasibilityEnquirySidebar";
 import useSidebar from "@/hooks/useSidebar";
+import FeasibilityEnquirySidebar from "@/app/[locale]/(logged-out)/search/components/FeasibilityEnquirySidebar";
 
-// import { CategoryHeader } from "./DatasetQuickViewDialog.styles";
 
 const TRANSLATION_PATH = "modules.dialogs.FeasibilityEnquiryDialog";
 
@@ -18,16 +16,22 @@ interface DatasetQuickViewDialogProps {
     result: SearchResultDataset;
 }
 
-
-
 const FeasibilityEnquiryDialog = ({ result }: DatasetQuickViewDialogProps) => {
     const t = useTranslations(TRANSLATION_PATH);
     const { hideDialog } = useDialog();
     const { showSidebar } = useSidebar();
     console.log(result);
 
-    const datasets = [result];
     const handleFeasibilityEnquiries = () => {
+        const datasets = [
+            {
+                datasetId: Number(result._id),
+                name: result.metadata.summary.title,
+                teamId: result.team.id,
+                teamName: result.team.name,
+                teamMemberOf: result.team.member_of,
+            },
+        ];
         showSidebar({
             title: "Messages",
             content: <FeasibilityEnquirySidebar datasets={datasets} />,
@@ -40,17 +44,28 @@ const FeasibilityEnquiryDialog = ({ result }: DatasetQuickViewDialogProps) => {
             // title={"Title"}
             onClose={() => hideDialog()}>
             <MuiDialogContent sx={{ paddingX: 8 }}>
-                <Box sx={{
+                <Box
+                    sx={{
                         display: "grid",
                         gridTemplateColumns: "repeat(2, 1fr)",
                     }}>
-                    <Button sx={{display: 'flex'}} variant="contained" onClick={handleFeasibilityEnquiries}>{t("enquire")}</Button>
+                    <Button
+                        sx={{ display: "flex" }}
+                        variant="contained"
+                        onClick={handleFeasibilityEnquiries}>
+                        {t("enquire")}
+                    </Button>
                     {/* <Divider
                         orientation="vertical"
                         variant="middle"
                         color="black"
                     /> */}
-                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: "center" }}>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}>
                         <Typography variant="h3" mb={2}>
                             {
                                 "BLAH BLAH6To make a feasibility enquiry for multiple datasets:\n1. Add datasets to your library2. Generate a feasibility enquiry from your library page"
