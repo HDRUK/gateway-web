dfb18ad63b8472183ce70712e972654bfd822403
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-09-02 13:35:55 +0100

    fix: linting error

--- src/app/[locale]/[...not_found]/page.tsx
+++ src/app/[locale]/[...not_found]/page.tsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { redirect } from "next/navigation";
-import ErrorDisplay from "@/components/ErrorDisplay";
 import { RouteName } from "@/consts/routeName";
 
 const NotFound = () => {
