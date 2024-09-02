6ae5bfdeee0d0795869a7825a0e6a6a33a834f22
Phil Reeks <phil.reeks@hdruk.ac.uk>
2024-09-02 14:39:22 +0100

    Redirect to cohort discovery if user has GENERAL_ACCESS permission

--- src/app/[locale]/(logged-out)/about/cohort-discovery/components/CtaOverride/CtaOverride.tsx
+++ src/app/[locale]/(logged-out)/about/cohort-discovery/components/CtaOverride/CtaOverride.tsx
@@ -6,16 +6,22 @@ import Button from "@/components/Button";
 import ProvidersDialog from "@/modules/ProvidersDialog";
 import useAuth from "@/hooks/useAuth";
 import useDialog from "@/hooks/useDialog";
+import { getPermissions } from "@/utils/permissions";
 
 export const DATA_TEST_ID = "cta-override-button";
+const COHORT_DISCOVERY_PERMISSION = "GENERAL_ACCESS";
+const COHORT_DISCOVERY_URL = "https://rquest.prod.healthdatagateway.org/";
 
 const CtaOverride = ({ ctaLink }: { ctaLink: CtaLink }) => {
     const { showDialog } = useDialog();
     const { push } = useRouter();
-    const { isLoggedIn } = useAuth();
+    const { isLoggedIn, user } = useAuth();
+    const permissions = getPermissions(user?.roles);
 
     const handleCtaClick = () => {
-        if (isLoggedIn) {
+        if (permissions[COHORT_DISCOVERY_PERMISSION]) {
+            push(COHORT_DISCOVERY_URL);
+        } else if (isLoggedIn) {
             push(ctaLink.url);
         } else {
             showDialog(ProvidersDialog, { isProvidersDialog: true });
