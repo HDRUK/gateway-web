8955ad87e094ab7758f6f80ad010cf094a70c271
lewissaunders11 <lewis.saunders@hdruk.ac.uk>
2024-09-09 08:34:30 +0000

    exclude download results from specific search categories

--- src/app/[locale]/(logged-out)/search/components/Search/Search.tsx
+++ src/app/[locale]/(logged-out)/search/components/Search/Search.tsx
@@ -525,6 +525,13 @@ const Search = ({ filters }: SearchProps) => {
                 return t("searchExplainerDatasets");
         }
     };
+
+    const excludedDownloadSearchCategories = [
+        SearchCategory.PUBLICATIONS,
+        SearchCategory.DATA_PROVIDERS,
+        SearchCategory.COLLECTIONS,
+    ];
+
     const showPublicationWelcomeMessage =
         !isSearching &&
         !queryParams.query &&
@@ -575,15 +582,19 @@ const Search = ({ filters }: SearchProps) => {
                             sortOptions={getSortOptions()}
                         />
                     </Box>
-                    <Button
-                        onClick={() =>
-                            !isDownloading && downloadSearchResults()
-                        }
-                        variant="text"
-                        startIcon={<DownloadIcon />}
-                        disabled={isDownloading || !data?.list?.length}>
-                        {t("downloadResults")}
-                    </Button>
+                    {!excludedDownloadSearchCategories.includes(
+                        queryParams.type
+                    ) && (
+                        <Button
+                            onClick={() =>
+                                !isDownloading && downloadSearchResults()
+                            }
+                            variant="text"
+                            startIcon={<DownloadIcon />}
+                            disabled={isDownloading || !data?.list?.length}>
+                            {t("downloadResults")}
+                        </Button>
+                    )}
                     <Button
                         variant="outlined"
                         color="secondary"
