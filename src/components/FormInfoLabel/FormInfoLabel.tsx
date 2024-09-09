ed3fe5657dc25ba7e4151d44c08d4708cef13806
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2024-09-09 14:50:15 +0100

    hotifx- tbody

--- src/components/FormInfoLabel/FormInfoLabel.tsx
+++ src/components/FormInfoLabel/FormInfoLabel.tsx
@@ -35,7 +35,6 @@ const FormInfoLabel = ({
                     }}
                 />
             )}
-            <tbody />
 
             {!horizontalForm && info && (
                 <FormHelperText
