c24686b13b4f94b2d97c2e71f62203fd32b98133
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2024-09-09 19:03:24 +0100

    linter

--- src/app/[locale]/components/RichResults/Breadcrumbs.test.tsx
+++ src/app/[locale]/components/RichResults/Breadcrumbs.test.tsx
@@ -1,82 +1,77 @@
+import { screen, render } from "@testing-library/react";
+import BreadCrumbs from "./Breadcrumbs";
 
-import {screen, render} from '@testing-library/react'
-import BreadCrumbs from './Breadcrumbs';
+const mockUsePathname = jest.fn();
+const locale = "en";
+const hostname = "https://wwww.locahost:3000.com";
 
-const mockUsePathname = jest.fn()
-const locale = 'en'
-const hostname = 'https://wwww.locahost:3000.com'
-const fullDomain = hostname + '/' + locale
-
-Object.defineProperty(window, 'location', {
-  value: {
-    get origin() {
-      return hostname;
+Object.defineProperty(window, "location", {
+    value: {
+        get origin() {
+            return hostname;
+        },
     },
-  },
 });
 
+jest.mock("next/script", () => {
+    // eslint-disable-next-line react/display-name, react/prop-types
+    return ({ children, ...props }) => {
+        return (
+            <script data-testid="breadCrumbs" {...props}>
+                {children}
+            </script>
+        );
+    };
+});
 
-jest.mock('next/script', () => {
-  // eslint-disable-next-line react/display-name, react/prop-types
-  return function ({ children, ...props }) {
-    return (
-      <script data-testid='breadCrumbs' {...props}>
-        {children}
-      </script>
-    )
-  }
-})
-
-jest.mock('next/navigation', () => ({
-  usePathname() {
-    return mockUsePathname()
-  },
-  useParams() {
-  return {
-    locale: locale
-  }
-  }
-}))
+jest.mock("next/navigation", () => ({
+    usePathname() {
+        return mockUsePathname();
+    },
+    useParams() {
+        return {
+            locale,
+        };
+    },
+}));
 
 interface setUpProps {
-  pathName: string
+    pathName: string;
 }
 
 const setUp = async ({ pathName }: setUpProps) => {
-  mockUsePathname.mockImplementation(() => '/'+locale+ pathName)
+    mockUsePathname.mockImplementation(() => `/${locale}${pathName}`);
 
-  render(<BreadCrumbs />)
-  return await screen.findByTestId('breadCrumbs')
-}
+    render(<BreadCrumbs />);
+    return await screen.findByTestId("breadCrumbs");
+};
 
 const testCases = [
-  {
-    url: '',
-    length: 1,
-    name: ['Home']
-  },
-  {
-    url: '/about',
-    length: 2,
-    name: ['Home', 'About']
-  },
-  {
-    url: '/about/data',
-    length: 3,
-    name: ['Home', 'About', 'Data']
-  }
-]
+    {
+        url: "",
+        length: 1,
+        name: ["Home"],
+    },
+    {
+        url: "/about",
+        length: 2,
+        name: ["Home", "About"],
+    },
+    {
+        url: "/about/data",
+        length: 3,
+        name: ["Home", "About", "Data"],
+    },
+];
 
-describe('Should generate breadcrumb data for', ()=>{
+describe("Should generate breadcrumb data for", () => {
+    it.each(testCases)("$url", async ({ url, length, name }) => {
+        const scriptElement = await setUp({ pathName: url });
+        const config = JSON.parse(scriptElement.innerHTML);
 
-  it.each(testCases)('$url', async({url, length, name})=>{
-    const scriptElement = await setUp({ pathName: url })
-    const config = JSON.parse(scriptElement.innerHTML)
-
-    expect(config.itemListElement.length).toEqual(length)
-    config.itemListElement.map((obj:any, index: number)=>{
-      expect(obj.item.name).toEqual(name[index])
-    })
-  })
-  
-})
+        expect(config.itemListElement.length).toEqual(length);
+        config.itemListElement.forEach((obj, index) => {
+            expect(obj.item.name).toEqual(name[index]);
+        });
+    });
+});
--- src/app/[locale]/components/RichResults/Breadcrumbs.tsx
+++ src/app/[locale]/components/RichResults/Breadcrumbs.tsx
@@ -1,95 +1,91 @@
-'use client'
+"use client";
 
-import { BreadcrumbList, WithContext } from 'schema-dts'
-import Script from 'next/script'
-import { usePathname, useParams } from 'next/navigation'
+import { usePathname, useParams } from "next/navigation";
+import Script from "next/script";
+import { BreadcrumbList, WithContext } from "schema-dts";
 
 interface itemType {
-  '@id': string
-  name: string
+    "@id": string;
+    name: string;
 }
 
 interface itemListType {
-  '@type': 'ListItem'
-  position: number
-  item: itemType
+    "@type": "ListItem";
+    position: number;
+    item: itemType;
 }
 
 const BreadCrumbs = () => {
     const { locale } = useParams();
     const origin =
-    typeof window !== 'undefined' && window.location.origin
-        ? window.location.origin
-        : '';
+        typeof window !== "undefined" && window.location.origin
+            ? window.location.origin
+            : "";
     const pathname = usePathname();
-    const mainDomain = origin + '/' + locale;
+    const mainDomain = `${origin}/${locale}`;
 
-  let position = 1
-  const listItems: itemListType[] = [
-    {
-      '@type': 'ListItem',
-      position: position,
-      item: {
-        '@id': mainDomain,
-        name: 'Home'
-      }
-    }
-  ]
+    let position = 1;
+    const listItems: itemListType[] = [
+        {
+            "@type": "ListItem",
+            position,
+            item: {
+                "@id": mainDomain,
+                name: "Home",
+            },
+        },
+    ];
 
-  const isHome = pathname === + '/' + locale
+    const isHome = pathname === +"/" + locale;
 
-  if (!isHome) {
-    const splitPaths = pathname!.split('/');
-    const getPathForSub = (
-      position: number,
-      size: number
-    ) => {
-      const length = position + 1
-      const isFirstOrOnlyItem = (size === length) || (length === 1)
-      if (isFirstOrOnlyItem) {
-        return origin + pathname
-      } else {
-        let url = origin
+    if (!isHome) {
+        const splitPaths = pathname!.split("/");
+        const getPathForSub = (position: number, size: number) => {
+            const length = position + 1;
+            const isFirstOrOnlyItem = size === length || length === 1;
+            if (isFirstOrOnlyItem) {
+                return origin + pathname;
+            }
+            let url = origin;
+            splitPaths.shift(); // removes ''
+            splitPaths.forEach((path, index) => {
+                if (index + 1 < size) {
+                    url += `/${path}`;
+                }
+            });
+            return url;
+        };
         splitPaths.shift(); // removes ''
-        splitPaths.map((path, index) => {
-          if (index + 1 < size) {
-            url += '/' + path
-          }
-        })
-        return url
-      }
-    }
-    splitPaths.shift(); // removes ''
-    if (locale){
-        splitPaths.shift(); // removes 'en'
-    }
-   
-    splitPaths.map((path: string, index, row) => {
-      position++
-      const name = path.replace('-', ' ')
-      const item: itemListType = {
-        '@type': 'ListItem',
-        position: position,
-        item: {
-          '@id': getPathForSub(index, row.length),
-          name: name.charAt(0).toUpperCase() + name.slice(1)
+        if (locale) {
+            splitPaths.shift(); // removes 'en'
         }
-      }
-      listItems.push(item)
-    })
-  }
 
-  const jsonLd: WithContext<BreadcrumbList> = {
-    '@context': 'https://schema.org',
-    '@type': 'BreadcrumbList',
-    itemListElement: listItems
-  }
+        splitPaths.forEach((path: string, index, row) => {
+            position += 1;
+            const name = path.replace("-", " ");
+            const item: itemListType = {
+                "@type": "ListItem",
+                position,
+                item: {
+                    "@id": getPathForSub(index, row.length),
+                    name: name.charAt(0).toUpperCase() + name.slice(1),
+                },
+            };
+            listItems.push(item);
+        });
+    }
 
-  return (
-    <Script id="breadcrumbs" type="application/ld+json">
-      {JSON.stringify(jsonLd)}
-    </Script>
-  )
-}
+    const jsonLd: WithContext<BreadcrumbList> = {
+        "@context": "https://schema.org",
+        "@type": "BreadcrumbList",
+        itemListElement: listItems,
+    };
 
-export default BreadCrumbs
+    return (
+        <Script id="breadcrumbs" type="application/ld+json">
+            {JSON.stringify(jsonLd)}
+        </Script>
+    );
+};
+
+export default BreadCrumbs;
