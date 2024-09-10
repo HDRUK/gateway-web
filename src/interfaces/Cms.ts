a8a8ec3ced97f7c5746ebef314825fea8f409ded
lewissaunders11 <lewis.saunders@hdruk.ac.uk>
2024-09-10 07:46:21 +0000

    add bottomPanel to template

--- src/interfaces/Cms.ts
+++ src/interfaces/Cms.ts
@@ -35,6 +35,7 @@ interface PageTemplatePromo {
             topRightPanel?: string;
             topLeftPanel: string;
             middlePanel: string;
+            bottomPanel?: string;
         };
     };
     ctaOverrideComponent?: React.ReactElement;
