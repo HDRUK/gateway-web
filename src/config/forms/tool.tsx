40c32ac468ff9ddcd1de1e450367d59ba7483392
Sam Cox <sam.cox@nottingham.ac.uk>
2024-09-10 15:33:00 +0100

    fix validation of results_insights.

--- src/config/forms/tool.tsx
+++ src/config/forms/tool.tsx
@@ -30,7 +30,7 @@ const validationSchema = yup.object().shape({
     name: yup.string().required().label("Name of script, tool or software"),
     type_category: yup.array().of(yup.number()).required().label("Category"),
     description: yup.string().min(50).max(1500).required().label("Description"),
-    results: yup
+    results_insights: yup
         .string()
         .min(50)
         .max(1500)
