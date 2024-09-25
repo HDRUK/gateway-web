291f39ba050d1aed0e164fd1962891835a756777
lewissaunders11 <lewis.saunders@hdruk.ac.uk>
2024-09-25 10:03:21 +0000

    edit collection page

---
+++ src/app/[locale]/account/team/[teamId]/(withLeftNav)/collections/[collectionId]/page.tsx
@@ -0,0 +1,35 @@
+import { cookies } from "next/headers";
+import ProtectedAccountRoute from "@/components/ProtectedAccountRoute";
+import { getTeam, getUser } from "@/utils/api";
+import { getPermissions } from "@/utils/permissions";
+import { getTeamUser } from "@/utils/user";
+import CreateCollection from "./components";
+
+export const metadata = {
+    title: "Health Data Research Innovation Gateway - My Account - Create Collection",
+    description: "",
+};
+
+export default async function CollectionCreatePage({
+    params,
+}: {
+    params: { teamId: string; collectionId: string };
+}) {
+    const { teamId, collectionId } = params;
+    const cookieStore = cookies();
+    const user = await getUser(cookieStore);
+    const team = await getTeam(cookieStore, teamId);
+    const teamUser = getTeamUser(team?.users, user?.id);
+    const permissions = getPermissions(user.roles, teamUser?.roles);
+
+    return (
+        <ProtectedAccountRoute
+            permissions={permissions}
+            pagePermissions={["collections.update"]}>
+            <CreateCollection
+                teamId={teamId}
+                collectionId={collectionId}
+            />
+        </ProtectedAccountRoute>
+    );
+}
