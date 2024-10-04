4f5b4b7a60d5484ac2e44c15178b26ba8fec96bc
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2024-10-04 10:06:44 +0100

    changes

--- src/components/Encoder/index.tsx
+++ src/components/Encoder/index.tsx
@@ -2,13 +2,17 @@ import DOMPurify from "isomorphic-dompurify";
 
 interface EncoderProps {
     raw: string;
+    component?: React.FunctionComponent;
 }
 
-const Encoder = ({ raw }: EncoderProps) => (
-    <div
-        dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(raw),
-        }}
-    />
-);
+const Encoder = ({ raw, component }: EncoderProps) => {
+    const Component = component || "div";
+    return (
+        <Component
+            dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(raw),
+            }}
+        />
+    );
+};
 export default Encoder;
