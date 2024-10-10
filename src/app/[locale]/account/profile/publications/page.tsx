92b00001a3b90633b938f4ce43af2413b72adf4d
Sam Cox <sam.cox@nottingham.ac.uk>
2024-10-10 12:45:25 +0100

    Give individual users the permissions required to update and delete publications, so those actions show on the publications list page.

--- src/app/[locale]/account/profile/publications/page.tsx
+++ src/app/[locale]/account/profile/publications/page.tsx
@@ -19,6 +19,11 @@ export default async function UserPublicationsPage({
     const permissions = getPermissions(user.roles);
     const userId = user?.id?.toString();
 
+    // manually add papers permissions for individual users so that they have permissions on their owned papers.
+    ["papers.update", "papers.delete"].forEach(
+        value => (permissions[value] = true)
+    );
+
     return (
         <UserPublications
             permissions={permissions}
