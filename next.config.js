bf1147f2da412df15cecd9a321d97d354948e8f5
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-10-17 17:09:32 +0100

    fix: add debugging for webpack build failure

--- next.config.js
+++ next.config.js
@@ -38,6 +38,10 @@ const nextConfig = withNextIntl({
             },
         ],
     },
+    webpack(config) {
+        config.infrastructureLogging = { debug: /PackFileCache/ };
+        return config;
+    },
     async redirects() {
         return [
             {
