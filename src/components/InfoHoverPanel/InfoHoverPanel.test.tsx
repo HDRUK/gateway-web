a793e65902476bed32fe50626ec49760be98143c
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-10-21 13:44:09 +0100

    fix: replace some upper case entity types

--- src/components/InfoHoverPanel/InfoHoverPanel.test.tsx
+++ src/components/InfoHoverPanel/InfoHoverPanel.test.tsx
@@ -39,7 +39,7 @@ describe("InfoHoverPanel", () => {
         await waitFor(() => {
             expect(
                 screen.getByText(
-                    "Find Data Uses / Research Projects projects: Search and filter Data Uses / Research Projects projects related to specific datasets and other features"
+                    "Find Data Uses / Research Projects: search and filter data uses / research projects /studies related to specific data sets and other features."
                 )
             ).toBeInTheDocument();
         });
