b26d570666933361a1c358ff8161402310dd3dd6
Calum Macdonald <calmacx@gmail.com>
2024-11-04 11:53:23 +0000

    revert file

--- src/app/[locale]/account/team/[teamId]/(withoutLeftNav)/datasets/components/SubmissionScreen/SubmissionScreen.tsx
+++ src/app/[locale]/account/team/[teamId]/(withoutLeftNav)/datasets/components/SubmissionScreen/SubmissionScreen.tsx
@@ -41,8 +41,7 @@ const SubmissionScreen = ({
             return;
         }
         const checkFormValidation = async () => {
-            const isValid = await trigger();
-            setIsValid(isValid);
+            setIsValid(await trigger());
         };
 
         checkFormValidation();
