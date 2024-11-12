3c828d4a62e7a6e8910de818fb280c6b2fa07ac1
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2024-11-12 14:22:22 +0000

    error

--- src/components/Wysiwyg/consts.ts
+++ src/components/Wysiwyg/consts.ts
@@ -9,7 +9,6 @@ import OrderedList from "@tiptap/extension-ordered-list";
 import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
 import Underline from "@tiptap/extension-underline";
-import Link from "@tiptap/extension-link";
 
 const EXTENSIONS = [
     Document,
@@ -23,7 +22,6 @@ const EXTENSIONS = [
     OrderedList,
     BulletList,
     ListItem,
-    Link
 ];
 
 export { EXTENSIONS };
