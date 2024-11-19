d78fe442e2bb43fbf579fbe983ff21810080d63b
Sam Cox <sam.cox@nottingham.ac.uk>
2024-11-19 17:00:37 +0000

    Lint

--- src/app/[locale]/account/profile/collections/create/page.tsx
+++ src/app/[locale]/account/profile/collections/create/page.tsx
@@ -1,9 +1,7 @@
 import { cookies } from "next/headers";
 import CollectionForm from "@/components/CollectionForm";
-import ProtectedAccountRoute from "@/components/ProtectedAccountRoute";
 import { getUser } from "@/utils/api";
 import metaData, { noFollowRobots } from "@/utils/metadata";
-import { getPermissions } from "@/utils/permissions";
 
 export const metadata = metaData(
     {
@@ -18,7 +16,5 @@ export default async function CollectionCreatePage() {
     const user = await getUser(cookieStore);
     const userId = user?.id?.toString();
 
-    return (
-        <CollectionForm userId={userId} />
-    );
+    return <CollectionForm userId={userId} />;
 }
