9889ec2ca133c2a519c89b5c651a9659ed904ded
Sam Cox <sam.cox@nottingham.ac.uk>
2024-12-06 12:27:32 +0000

    Use Dataset as the type as soon as possible - just transfer shortTitle to it.

--- src/components/CollectionForm/CollectionForm.tsx
+++ src/components/CollectionForm/CollectionForm.tsx
@@ -174,6 +174,7 @@ const CollectionForm = ({
                 return {
                     id: dv.dataset_id,
                     latest_metadata: { metadata: dv.metadata },
+                    shortTitle: dv.shortTitle,
                 };
             });
             return tempDatasets;
@@ -227,6 +228,19 @@ const CollectionForm = ({
 
     const watchAll = watch();
 
+    const datasetToDatasetVersion = (datasets: ReducedDataset[]) => {
+        if (!datasets) return [];
+
+        const tempDatasetVersions = datasets.map(d => {
+            return {
+                id: d.version_id,
+                dataset_id: d.id,
+                shortTitle: d.shortTitle,
+            };
+        });
+        return tempDatasetVersions;
+    };
+
     const handleAddResource = () => {
         showDialog(AddResourceDialog, {
             setResources: (selectedResources: SelectedResources) => {
@@ -238,6 +252,7 @@ const CollectionForm = ({
                     "datasets",
                     selectedResources[ResourceType.DATASET] as ReducedDataset[]
                 );
+                // setValue("dataset_versions", datasetToDatasetVersion(selectedResources[ResourceType.DATASET]) as VersionItem[]);
                 setValue(
                     "publications",
                     selectedResources[ResourceType.PUBLICATION] as Publication[]
@@ -265,18 +280,22 @@ const CollectionForm = ({
         const tempDatasets = datasetVersions.map(dv => {
             return {
                 id: dv.dataset_id,
+                version_id: dv.id,
                 shortTitle: dv.shortTitle,
             };
         });
         return tempDatasets;
     };
 
+
+
     const selectedResources = useMemo(() => {
+        console.log('memo', getValues("datasets"));
         return {
             datause: (getValues("dur") as DataUse[]) || [],
             publication: (getValues("publications") as Publication[]) || [],
             tool: (getValues("tools") as Tool[]) || [],
-            dataset: (datasetVersionToDataset(getValues("dataset_versions")) as ReducedDataset[]) || [],
+            dataset: (getValues("datasets") as ReducedDataset[]) || [],
         };
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [watchAll, getValues]);
@@ -300,7 +319,14 @@ const CollectionForm = ({
         } else if (resourceType === ResourceType.TOOL) {
             setValue("tools", updatedResources as Tool[]);
         } else if (resourceType === ResourceType.DATASET) {
-            setValue("dataset_versions", updatedResources as ReducedDataset[]);
+            // convert dataset ids to dataset_version_ids
+            // const dataset_id = 
+            // set 
+            // console.log('removed', updatedResources,  datasetToDatasetVersion(updatedResources));
+            setValue("datasets", updatedResources as ReducedDataset[]);
+            // setValue("dataset_versions", datasetToDatasetVersion(updatedResources) as ReducedDataset[]);
+            // console.log('getValues("dataset_versions")', getValues("dataset_versions"));
+            // console.log('getValues("datasets")', getValues("datasets"));
         }
     };
 
