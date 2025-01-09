5e2e6c0cf04ea382207b39084fa48a382a7dfa30
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-01-09 10:49:09 +0000

    hotifx: (GAT-5913) Collections logic in other places (#949)
    * hotifx: (GAT-5913) Collections logic in other places
    
    * missing import

--- src/app/[locale]/account/profile/collections/create/page.tsx
+++ src/app/[locale]/account/profile/collections/create/page.tsx
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 import CollectionForm from "@/components/CollectionForm";
-import { getUser } from "@/utils/api";
+import { getKeywords, getUser } from "@/utils/api";
 import metaData, { noFollowRobots } from "@/utils/metadata";
 
 export const metadata = metaData(
@@ -15,6 +15,12 @@ export default async function CollectionCreatePage() {
     const cookieStore = cookies();
     const user = await getUser(cookieStore);
     const userId = user?.id?.toString();
-
-    return <CollectionForm userId={userId} />;
+    const keywords = await getKeywords(cookieStore);
+    const keywordOptions = keywords.map(data => {
+        return {
+            value: data.id as ValueType,
+            label: data.name,
+        };
+    });
+    return <CollectionForm userId={userId} keywordOptions={keywordOptions} />;
 }
