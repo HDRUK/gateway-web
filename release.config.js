ee2dc4f365b6c7dd21aa14fa4a9bf2f1e52ecb80
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-01-29 11:00:28 +0000

    config

--- release.config.js
+++ release.config.js
@@ -1,6 +1,27 @@
 const fs = require("fs");
 const path = require("path");
 
+function updateChartYamlVersion(newVersion) {
+  try {
+
+
+  const chartFilePath = path.resolve(__dirname, "chart/Chart.yaml");
+  const chartContent = fs.readFileSync(chartFilePath, "utf8");
+
+  const updatedContent = chartContent.replace(
+    /^version:\s*[0-9]+\.[0-9]+\.[0-9]+/m,
+    `version: ${newVersion}`
+  );
+
+
+  fs.writeFileSync(chartFilePath, updatedContent, "utf8");
+  console.log(`Updated chart/Chart.yaml to version ${newVersion}`);
+} catch(e){
+  console.log(e)
+  process.exit(1)
+}
+}
+
 module.exports = {
   branches: ["chore/GAT-6176"],
   plugins: [
