64d9393ad31ac464afe0581a02c559a3dc60b563
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-01-29 15:50:41 +0000

    feat(GAT-0000): This is a really big change

--- release.config.js
+++ release.config.js
@@ -12,47 +12,25 @@ module.exports = {
             { type: "fix", section: "🐛 Bug Fixes" },
             { type: "perf", section: "⚡ Performance Improvements" },
             { type: "docs", section: "📖 Documentation", hidden: false },
-            { type: "chore", section: "🔧 Maintenance", hidden: false },
+            { type: "chore", section: "🔧 Maintenance", hidden: true },
           ],
-        },
-        writerOpts: {
-          transform: (commit, _context) => {
-            if (!commit || typeof commit !== "object") return commit;
-
-            const jiraBaseUrl = process.env.JIRA_URL
-            const jiraRegex = /\b([A-Z]+-\d+)\b/g;
-
-              const newCommit = { ...commit };
-
-            // Ensure the commit has a valid date
-            if (typeof newCommit.date === "string") {
-              newCommit.date = new Date(newCommit.date);
-            }
-
-            if (newCommit.subject && typeof newCommit.subject === "string") {
-              newCommit.subject = newCommit.subject.replace(
-                jiraRegex,
-                `[$1](${jiraBaseUrl}$1)`
-              );
-            }
-
-            return newCommit;
-          },
-        },
-      },
+          issuePrefixes: ["GAT", "TPD", "OS"], // Jira prefixes
+          issueUrlFormat: process.env.JIRA_URL + "{{prefix}}{{id}}"
+        }
+      }
     ],
     [
       "@semantic-release/changelog",
       {
         changelogFile: "CHANGELOG.md",
-      },
+      }
     ],
     "@semantic-release/github",
     [
       "@semantic-release/exec",
       {
-        prepareCmd: "node updateVersions.js ${nextRelease.version} && git add chart/gateway-web/Chart.yaml package.json",
-      },
+        prepareCmd: "node updateVersions.js ${nextRelease.version} && git add chart/gateway-web/Chart.yaml package.json"
+      }
     ],
     [
       "@semantic-release/git",
@@ -61,11 +39,10 @@ module.exports = {
           "package.json",
           "package-lock.json",
           "CHANGELOG.md",
-          "chart/gateway-web/Chart.yaml",
+          "chart/gateway-web/Chart.yaml"
         ],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
-  ],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ]
 };
