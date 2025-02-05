b571b7e59414ac4c55c455fbbef04aee0ed944ec
Phil Reeks <phil.reeks@hdruk.ac.uk>
2025-02-05 10:40:40 +0000

    Add data access request utils and tests

---
+++ src/utils/dataAccessRequest.tsx
@@ -0,0 +1,70 @@
+import {
+    DarApplicationQuestion,
+    DarFormattedField,
+} from "@/interfaces/DataAccessRequest";
+import { inputComponents } from "@/config/forms";
+
+const mapKeysToValues = (keys: string[], valuesArray: (string | undefined)[]) =>
+    Object.fromEntries(keys.map((key, index) => [key, valuesArray[index]]));
+
+const calculateQuestionCount = (
+    filteredData: DarFormattedField[],
+    parentValues: {
+        [k: string]: string | undefined;
+    }
+) =>
+    filteredData.reduce((count, field) => {
+        let questionCount = 1;
+
+        // Check if the question has children that should be displayed
+        if (field.options?.length) {
+            const selectedOption = parentValues[field.question_id];
+
+            // Find all visible child questions
+            const visibleChildren = field.options.flatMap(
+                option =>
+                    option.children?.filter(
+                        child => selectedOption === option.label
+                    ) || []
+            );
+
+            questionCount += visibleChildren.length;
+        }
+
+        return count + questionCount;
+    }, 0);
+
+const formatDarQuestion = (
+    field: DarApplicationQuestion
+): DarFormattedField => ({
+    name: field.title,
+    component: field.component,
+    required: field.required,
+    question_id: field.question_id,
+    section_id: field.section_id,
+    options:
+        field.options?.map(option => ({
+            label: option.label,
+            value: option.label,
+            children:
+                (option.children &&
+                    Object.values(option.children)
+                        .flat()
+                        .map(formatDarQuestion)) ||
+                [],
+        })) || [],
+    ...(field.component === inputComponents.RadioGroup && {
+        radios: field.options?.map(option => ({
+            label: option.label,
+            value: option.label,
+        })),
+    }),
+    ...(field.component === inputComponents.CheckboxGroup && {
+        checkboxes: field.options?.map(option => ({
+            label: option.label,
+            value: option.label,
+        })),
+    }),
+});
+
+export { calculateQuestionCount, mapKeysToValues, formatDarQuestion };
