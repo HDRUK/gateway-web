f661e2bf90cd46062131e2d0f5cea38ac5d8b8f2
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-02-21 08:38:36 +0000

    fix(GAT-6303): Allow dev only

--- src/app/[locale]/account/team/[teamId]/(withLeftNav)/team-management/page.tsx
+++ src/app/[locale]/account/team/[teamId]/(withLeftNav)/team-management/page.tsx
@@ -24,11 +24,15 @@ export default async function TeamManagementPage({
     const team = await getTeam(cookieStore, teamId);
     const teamUser = getTeamUser(team?.users, user?.id);
     const permissions = getPermissions(user.roles, teamUser?.roles);
+    const isOnlyDev =
+        teamUser?.roles?.length === 1 &&
+        teamUser.roles[0]?.name === "developer";
 
     return (
         <ProtectedAccountRoute
             permissions={permissions}
-            pagePermissions={["roles.read"]}>
+            pagePermissions={["roles.read"]}
+            allowDevOnly={isOnlyDev}>
             <TeamManagement permissions={permissions} team={team} />
         </ProtectedAccountRoute>
     );
