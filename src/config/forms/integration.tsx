ffcb2f86545ef7bac16999950b44e4f56bfcfbe0
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-03-05 16:10:23 +0000

    fix(GAT-6406): Allow minutes (#1032)
    Co-authored-by: Vikas00230 <Vikas.Rachakonda@hdruk.ac.uk>

--- src/config/forms/integration.tsx
+++ src/config/forms/integration.tsx
@@ -66,7 +66,6 @@ const formFields = [
         name: { minute: "run_time_minute", hour: "run_time_hour" },
         info: "Set the hour the synchronisations will take place daily, in 24-hr format",
         component: inputComponents.TextTime,
-        minProps: { disabled: true },
     },
     {
         label: "Base URL",
