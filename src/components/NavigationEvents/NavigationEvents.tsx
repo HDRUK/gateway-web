5f6ee626cf941b4b3a38d60642e07aadd110d54a
Phil Reeks <phil.reeks@hdruk.ac.uk>
2025-03-18 17:12:04 +0000

    Only clear action bar when page url is changed

--- src/components/NavigationEvents/NavigationEvents.tsx
+++ src/components/NavigationEvents/NavigationEvents.tsx
@@ -1,20 +1,19 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import useActionBar from "@/hooks/useActionBar";
 
 const NavigationEvents = () => {
     const { hideBar, store } = useActionBar();
     const pathname = usePathname();
-    const searchParams = useSearchParams();
 
     useEffect(() => {
         if (store.isVisible) {
             hideBar();
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [pathname, searchParams]);
+    }, [pathname]);
 
     return null;
 };
