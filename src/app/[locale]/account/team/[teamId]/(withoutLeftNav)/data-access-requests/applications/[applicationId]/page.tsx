508acf167e6a5755aaa1f01840753f19383869b1
philreekshdr <153518986+philreekshdr@users.noreply.github.com>
2025-04-17 17:10:51 +0100

    fix(GAT-6632): Tweaks to DAR application status changes (#1085)

--- src/app/[locale]/account/team/[teamId]/(withoutLeftNav)/data-access-requests/applications/[applicationId]/page.tsx
+++ src/app/[locale]/account/team/[teamId]/(withoutLeftNav)/data-access-requests/applications/[applicationId]/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { DarTeamApplication } from "@/interfaces/DataAccessRequestApplication";
 import ProtectedAccountRoute from "@/components/ProtectedAccountRoute";
 import {
     beforeYouBeginSection,
@@ -112,16 +113,11 @@ export default async function DarApplicationPage({
             approval_status: DarApplicationApprovalStatus.FEEDBACK,
         });
 
-        // Refetch application
-        darApplication = await getDarTeamApplication(
-            cookieStore,
-            applicationId,
-            teamId
-        );
-
-        teamApplication = darApplication?.teams?.find(
-            team => team.team_id === +teamId
-        );
+        // Find the specific team and override its approval_status
+        teamApplication = {
+            ...darApplication?.teams?.find(team => team.team_id === +teamId),
+            approval_status: DarApplicationApprovalStatus.FEEDBACK,
+        } as DarTeamApplication;
     }
 
     if (!teamApplication) {
