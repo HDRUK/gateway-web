5b9588bd0ab3d700bd040e04cfbc9613c27c7c85
Phil Reeks <phil.reeks@hdruk.ac.uk>
2025-04-23 15:03:01 +0100

    Update to util

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -23,17 +23,17 @@ const getDayjs = (date: string | Date) => {
 
 const formatDate = (date: string | Date, formatStr = "DD MMM YYYY") => {
     const convertedDate1 = getDayjs(date);
-    return convertedDate1.format(formatStr);
+    return convertedDate1?.format(formatStr);
 };
 
 const differenceInDays = (date1: string | Date, date2: string | Date) => {
     const convertedDate1 = getDayjs(date1);
     const convertedDate2 = getDayjs(date2);
 
-    return convertedDate1.diff(convertedDate2, "day");
+    return convertedDate1?.diff(convertedDate2, "day");
 };
 const getYear = (date: string | Date) => {
-    return getDayjs(date).year();
+    return getDayjs(date)?.year();
 };
 
 const yearToDayJsDate = (year: string) => {
