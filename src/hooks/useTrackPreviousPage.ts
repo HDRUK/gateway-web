26da6472c6e75a0bfeaf7b088c6634f5903d1d4b
Phil Reeks <phil.reeks@hdruk.ac.uk>
2025-04-29 15:55:27 +0100

    Tweak

--- src/hooks/useTrackPreviousPage.ts
+++ src/hooks/useTrackPreviousPage.ts
@@ -8,7 +8,9 @@ export const useTrackPreviousPage = () => {
     const pathname = usePathname();
     const searchParams = useSearchParams();
 
-    const fullPath = `${pathname}?${searchParams?.toString()}`;
+    const fullPath = `${pathname}${
+        searchParams?.toString() ? searchParams.toString() : ""
+    }`;
 
     useEffect(() => {
         const currentPage = sessionStorage.getItem("currentPage");
