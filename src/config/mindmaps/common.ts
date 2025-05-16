756c80bb2bbca01912e98226f4533fc149512054
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-05-16 09:27:25 +0100

    fix(GAT-6819): mind map tabbing, remove props that broke mouse

--- src/config/mindmaps/common.ts
+++ src/config/mindmaps/common.ts
@@ -50,10 +50,6 @@ export const nodeValueToRectNode = (
         type: "rect",
         position: { x, y },
         origin,
-        selectable: false,
-        draggable: false,
-        interactive: false,
-        edgesFocusable: false,
         focusable: false,
         data: {
             id: index,
