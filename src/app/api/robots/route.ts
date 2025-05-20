e0a01f29c6b2b94fa55d10cb91a36806faac71ed
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-05-20 11:46:36 +0100

    fix robots

---
+++ src/app/api/robots/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const BLOCK_ROBOTS = process.env.BLOCK_ROBOTS;
+  const NEXT_PUBLIC_GATEWAY_URL = process.env.NEXT_PUBLIC_GATEWAY_URL;
+  const isBlocked = BLOCK_ROBOTS === 'true';
+
+const body = isBlocked
+  ? [
+      'User-agent: *',
+      'Disallow: /',
+      'Sitemap:'
+    ].join('\n')
+  : [
+      'User-agent: *',
+      'Allow: /',
+      'Disallow: /account/',
+      'Disallow: /sign-in',
+      'Disallow: /search',
+      `Sitemap: ${NEXT_PUBLIC_GATEWAY_URL}/sitemap.xml`
+    ].join('\n');
+
+  return new NextResponse(body, {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+}
