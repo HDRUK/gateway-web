dea7c5e0e6f617790226cad589d5dadc263b18be
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-06-18 21:29:40 +0100

    lint

--- src/utils/getSessionCookie.ts
+++ src/utils/getSessionCookie.ts
@@ -6,6 +6,6 @@ import { sessionCookie } from "@/config/session";
 export const getSessionCookie = async () => {
     const cookieStore = await cookies();
     const session = cookieStore.get(sessionCookie);
-    const token = session?.value ?? 'N/A'
+    const token = session?.value ?? "N/A";
     return token;
 };
