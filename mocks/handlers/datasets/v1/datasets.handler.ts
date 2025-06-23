6ef849b7806f9fe11826a8ca5926fa220e99bacc
Phil Reeks <phil.reeks@hdruk.ac.uk>
2025-06-23 10:12:08 +0100

    tweak

--- mocks/handlers/datasets/v1/datasets.handler.ts
+++ mocks/handlers/datasets/v1/datasets.handler.ts
@@ -2,7 +2,7 @@ import { rest } from "msw";
 import { Dataset } from "@/interfaces/Dataset";
 import { PaginationType } from "@/interfaces/Pagination";
 import apis from "@/config/apis";
-import { datasetsV1 } from "@/mocks/data/dataset";
+import { datasetsV1, datasetV1 } from "@/mocks/data/dataset";
 
 const getDatasetsV1 = (data: Dataset[] = datasetsV1, status = 200) => {
     return rest.get(apis.datasetsV2Url, (req, res, ctx) => {
