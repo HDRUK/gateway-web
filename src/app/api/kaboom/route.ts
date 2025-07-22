aa0a814342c680b05b7a4ee72c10853f01661159
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-07-22 10:23:33 +0100

    tester

--- src/app/api/kaboom/route.ts
+++ src/app/api/kaboom/route.ts
@@ -1,17 +1,10 @@
 import { NextResponse } from "next/server";
-import { logger } from "@/utils/logger";
 
 export async function GET() {
     console.info("hello");
     console.warn("hello");
     console.error("hello");
-    
-    const sessionId = "test-session-123";
-    const location = "GET /api/kaboom";
 
-    logger.info("Everything is working fine", sessionId, location);
-    logger.warn("This might be a potential issue", sessionId, location);
-    logger.error(new Error("Kaboom! Something exploded"), sessionId, location);
 
     return NextResponse.json({ message: "jam" });
 }
