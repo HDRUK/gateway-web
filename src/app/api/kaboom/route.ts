85793c60db1f08066d05fcb8aef61a6401b2d034
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-07-22 10:41:24 +0100

    winston

--- src/app/api/kaboom/route.ts
+++ src/app/api/kaboom/route.ts
@@ -1,10 +1,15 @@
 import { NextResponse } from "next/server";
+import winston from "winston";
+
 
 export async function GET() {
-    console.info("hello");
-    console.warn("hello");
-    console.error("hello");
-
-
-    return NextResponse.json({ message: "jam" });
+const logger = winston.createLogger({
+ level: 'info', // Minimum log level
+ format: winston.format.json(), // Output format for logs
+ transports: [
+   new winston.transports.Console(), // Log to the console
+ ],
+});
+logger.log({ level: 'info', message: 'Hello world' });
+  return NextResponse.json({ message: "logged" });
 }
