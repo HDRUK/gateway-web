22f83a7670faa3ba5f65e2b58dc9c9fa384a4111
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-08-05 16:32:43 +0100

    test

---
+++ src/app/[locale]/(logged-out)/about/cohort-discovery/components/CohortDiscoveryButton/CohortDiscoveryButton.tsx
@@ -0,0 +1,112 @@
+import { useEffect, useMemo, useState } from "react";
+import { Tooltip } from "@mui/material";
+import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
+import { CtaLink } from "@/interfaces/Cms";
+import Button from "@/components/Button";
+import ProvidersDialog from "@/modules/ProvidersDialog";
+import useAuth from "@/hooks/useAuth";
+import { useCohortStatus } from "@/hooks/useCohortStatus";
+import useDialog from "@/hooks/useDialog";
+import { RouteName } from "@/consts/routeName";
+
+interface CohortDiscoveryButtonProps {
+    ctaLink: CtaLink;
+    showDatasetExplanatoryTooltip: boolean | null;
+    color?: string | null;
+    tooltipOverride?: string | null;
+    disabledOuter?: boolean;
+    clickedAction?: () => void;
+}
+
+export const DATA_TEST_ID = "cohort-discovery-button";
+
+const TRANSLATION_PATH_CTAOVERRIDE = "components.CohortDiscoveryButton";
+
+const CohortDiscoveryButton = ({
+    ctaLink,
+    showDatasetExplanatoryTooltip = false,
+    color = undefined,
+    tooltipOverride = "",
+    disabledOuter = false,
+    clickedAction,
+    ...restProps
+}: CohortDiscoveryButtonProps) => {
+    const { showDialog } = useDialog();
+    const { push } = useRouter();
+    const { isLoggedIn, user } = useAuth();
+    const { requestStatus, redirectUrl } = useCohortStatus(user?.id);
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    const t = useTranslations(TRANSLATION_PATH_CTAOVERRIDE);
+
+    const openAthensInvalid = useMemo(
+        () =>
+            isLoggedIn &&
+            user?.provider === "open-athens" &&
+            !user?.secondary_email,
+        [isLoggedIn, user]
+    );
+
+    useEffect(() => {
+        if (isClicked) {
+            setIsClicked(false);
+
+            if (clickedAction) {
+                clickedAction();
+            }
+
+            if (redirectUrl) {
+                push(redirectUrl);
+                console.log('redirectUrl', redirectUrl)
+            } else if (isLoggedIn) {
+                // check that if the user is using OpenAthens, that they have provided a secondary email,
+                // and send them to set it if not
+                if (openAthensInvalid) {
+                    push(`/${RouteName.ACCOUNT}/${RouteName.PROFILE}`);
+                } else {
+                    push(ctaLink.url);
+                     console.log('ctaLink.url', ctaLink.url)
+                }
+            } else {
+                showDialog(ProvidersDialog, { isProvidersDialog: true });
+            }
+        }
+    }, [redirectUrl, isClicked, isLoggedIn, clickedAction]);
+
+    const isDisabled =
+        isLoggedIn && requestStatus
+            ? !["APPROVED", "REJECTED", "EXPIRED"].includes(requestStatus)
+            : false;
+
+    const isApproved =
+        isLoggedIn && requestStatus && requestStatus === "APPROVED";
+
+    return (
+        <Tooltip
+            title={
+                tooltipOverride ||
+                (isDisabled
+                    ? t(`notApproved`)
+                    : openAthensInvalid
+                    ? t("setSecondaryEmail")
+                    : showDatasetExplanatoryTooltip && isApproved
+                    ? t("explanatoryTooltip")
+                    : "")
+            }>
+            <span>
+                <Button
+                    onClick={() => setIsClicked(true)}
+                    data-testid={DATA_TEST_ID}
+                    color={color}
+                    disabled={disabledOuter || isDisabled}
+                    {...restProps}>
+                    {ctaLink?.title}
+                </Button>
+            </span>
+        </Tooltip>
+    );
+};
+
+export default CohortDiscoveryButton;
