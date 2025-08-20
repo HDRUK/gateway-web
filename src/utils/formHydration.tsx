fb57a8ca128e04523c21c45687d33c14bacf0aa2
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-08-20 13:26:44 +0100

    lint

--- src/utils/formHydration.tsx
+++ src/utils/formHydration.tsx
@@ -409,12 +409,12 @@ const mapFormFieldsForSubmission = (
         set(formattedFormData, key, value);
     });
 
-    formattedFormData.provenance.origin.datasetType = formData["Dataset Type Array"].map(
-        item => ({
-            name: item["Dataset type"],
-            subTypes: item["Dataset subtypes"],
-        })
-    );
+    formattedFormData.provenance.origin.datasetType = formData[
+        "Dataset Type Array"
+    ].map(item => ({
+        name: item["Dataset type"],
+        subTypes: item["Dataset subtypes"],
+    }));
     const cleanUndefinedObjects = (
         obj: Record<string, unknown>
     ): Record<string, unknown> | undefined => {
