2875345ac9ac45c2422be21362078174204e2d9b
Tristan Millington <tristan.millington@gmail.com>
2025-09-29 14:24:25 +0100

    Added pagination

--- src/app/[locale]/account/team/[teamId]/(withLeftNav)/integrations/integration/list/[intId]/history/page.tsx
+++ src/app/[locale]/account/team/[teamId]/(withLeftNav)/integrations/integration/list/[intId]/history/page.tsx
@@ -1,27 +1,51 @@
+"use client";
+
+import { useState } from "react";
 import { Box } from "@mui/material";
 import { IntegrationHistory } from "@/interfaces/IntegrationHistory";
+import { PaginationType } from "@/interfaces/Pagination";
+import Pagination from "@/components/Pagination";
 import IntegrationHistoryTable from "../components/IntegrationHistoryTable";
 
 export default function IntegrationHistoryPage() {
-    const integrationHistory: Array<IntegrationHistory> = [
-        {
-            run_time: "12 March 2025 14:00",
-            success: false,
-            message: "An error occured",
-        },
-        {
-            run_time: "11 March 2025 14:00",
-            success: true,
-        },
-        {
-            run_time: "10 March 2025 14:00",
-            success: true,
-        },
-    ];
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const data: PaginationType<IntegrationHistory> = {
+        lastPage: 3,
+        to: 1,
+        from: 3,
+        currentPage: 1,
+        total: 3,
+        list: [
+            {
+                run_time: "12 March 2025 14:00",
+                success: false,
+                message: "An error occured",
+            },
+            {
+                run_time: "11 March 2025 14:00",
+                success: true,
+            },
+            {
+                run_time: "10 March 2025 14:00",
+                success: true,
+            },
+        ],
+    };
+
+    const { lastPage, list } = data || {};
 
     return (
         <Box sx={{ width: "100%" }}>
-            <IntegrationHistoryTable integrations={integrationHistory} />
+            <IntegrationHistoryTable integrations={list} />
+            <Pagination
+                isLoading={false}
+                page={currentPage}
+                count={lastPage}
+                onChange={(e: React.ChangeEvent<unknown>, page: number) =>
+                    setCurrentPage(page)
+                }
+            />
         </Box>
     );
 }
