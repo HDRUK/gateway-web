0c419e03a69e1506a42b46a4bf4c9858d8a8af3c
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-10-14 12:17:25 +0100

    lint

--- src/app/[locale]/(logged-out)/about/cohort-discovery-request/components/CohortDiscoveryRequestForm/CohortDiscoveryRequestForm.tsx
+++ src/app/[locale]/(logged-out)/about/cohort-discovery-request/components/CohortDiscoveryRequestForm/CohortDiscoveryRequestForm.tsx
@@ -62,10 +62,12 @@ const CohortDiscoveryRequestForm = ({
         if (!isUserLoading && !isAccessLoading) {
             if (accessData?.redirect_url) {
                 push(accessData.redirect_url);
-           } else if (
-            userData?.request_status &&
-            userData.request_status === "APPROVED"
-        ) {
+            } else if (
+                userData?.request_status &&
+                userData.request_status === "APPROVED"
+            ) {
+                push(`/${RouteName.ABOUT}/${RouteName.COHORT_DISCOVERY}`);
+            }
         }
     }, [isUserLoading, isAccessLoading, accessData, userData, push]);
 
