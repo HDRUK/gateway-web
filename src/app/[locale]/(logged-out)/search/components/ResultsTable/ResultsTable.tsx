e4000ac6402c0f830d0ffc7fb414ce8749a2006f
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-10-23 15:48:57 +0100

    fix(GAT-8113): Number.isNaN is strict, isNaN is less strict

--- src/app/[locale]/(logged-out)/search/components/ResultsTable/ResultsTable.tsx
+++ src/app/[locale]/(logged-out)/search/components/ResultsTable/ResultsTable.tsx
@@ -96,8 +96,8 @@ const getColumns = ({
         id: "dataProvider",
         cell: ({ row: { original } }) => {
             const dataCustodianId = get(original, PUBLISHERS_ID);
-            // if the below is false, its because the api has failed to find the team id based off the original uid for gatewayId
-            const isNumber = !Number.isNaN(dataCustodianId);
+                       // if the below is false, its because the api has failed to find the team id based off the original uid for gatewayId
+            const isNumber = !isNaN(dataCustodianId as number);
             const linkHref = `/${RouteName.DATA_CUSTODIANS_ITEM}/${dataCustodianId}`;
 
             return (
