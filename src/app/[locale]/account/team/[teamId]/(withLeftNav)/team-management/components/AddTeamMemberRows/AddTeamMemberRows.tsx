3f4876faa6659e65ea2fb6eef585dcb3aab2e545
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-11-07 17:18:54 +0000

    fix(GAT-0000): User Roles Loading conflict (#1370)

--- src/app/[locale]/account/team/[teamId]/(withLeftNav)/team-management/components/AddTeamMemberRows/AddTeamMemberRows.tsx
+++ src/app/[locale]/account/team/[teamId]/(withLeftNav)/team-management/components/AddTeamMemberRows/AddTeamMemberRows.tsx
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 import { Control, FieldArrayWithId, FieldValues } from "react-hook-form";
 import { IconButton } from "@mui/material";
 import { AddTeamMember, UserAndRoles } from "@/interfaces/AddTeamMember";
+import { AuthUser } from "@/interfaces/AuthUser";
 import { User } from "@/interfaces/User";
 import Box from "@/components/Box";
 import InputWrapper from "@/components/InputWrapper";
@@ -12,6 +13,8 @@ import {
     getRoleOptions,
 } from "@/config/forms/addTeamMember";
 import { AddIcon, RemoveIcon, SearchRoundedIcon } from "@/consts/icons";
+import { getPermissions } from "@/utils/permissions";
+import { getTeamUser } from "@/utils/user";
 import { getAvailableUsers } from "../AddTeamMemberDialog/AddTeamMemberDialog.utils";
 
 interface AddTeamMemberRowsProps<TFieldValues extends FieldValues> {
@@ -21,6 +24,7 @@ interface AddTeamMemberRowsProps<TFieldValues extends FieldValues> {
     remove: (fieldId: number) => void;
     append: (newRow: UserAndRoles) => void;
     onInputChangeUser?: (value: string) => void;
+    user: AuthUser;
     team: {
         users: User[];
     };
@@ -30,17 +34,24 @@ const AddTeamMemberRows = <TFieldValues extends FieldValues>({
     fields,
     control,
     team,
-    userPermissions,
+    user,
     remove,
     append,
     onInputChangeUser,
 }: AddTeamMemberRowsProps<TFieldValues>) => {
+    const teamUser = useMemo(() => {
+        return getTeamUser(team.users, user.id);
+    }, [team, user]);
+
+    const userPermissions = useMemo(() => {
+        return getPermissions(user.roles, teamUser?.roles);
+    }, [team, user]);
+
     // TODO: switch to typeahead propery
     // - difficult due to use of useFieldArray and shared options
     // - defaulting to full list of all users as a temporary measure..
     // const [searchName, setSearchName] = useState("");
     // const searchNameDebounced = useDebounce(searchName, 500);
-
     const { data: users = [], isLoading: isLoadingUsers } = useGet<User[]>(
         `${apis.usersV1Url}?mini`, // filterNames=${searchNameDebounced}`,
         {
