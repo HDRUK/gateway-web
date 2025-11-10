cceb718af2b7a37423eaf1f979ba603d884ea71f
Tristan Millington <tristan.millington@gmail.com>
2025-11-10 13:59:48 +0000

    fix tests

--- src/components/UploadFile/UploadFile.test.tsx
+++ src/components/UploadFile/UploadFile.test.tsx
@@ -57,7 +57,7 @@ describe("UploadFile Component", () => {
         );
         fireEvent.click(removeButton);
 
-        await waitFor(() => expect(onFileRemove).toHaveBeenCalledWith(1));
+        await waitFor(() => expect(onFileRemove).toHaveBeenCalledWith("977badec-cd56-438e-b468-196bf066fdb2"));
     });
 
     it("handles multiple file uploads", async () => {
