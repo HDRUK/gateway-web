16ab27a2438c41ac357355251dd12ffb6a43bc13
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-11-10 11:13:50 +0000

    allow anyone to talk to wigets

--- next.config.js
+++ next.config.js
@@ -55,6 +55,15 @@ const nextConfig = withNextIntl({
                     value: 'max-age=63072000; includeSubDomains; preload', 
                 }],
             },
+            {
+                source: "/api/widget/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
         ];
     },
     async rewrites() {
