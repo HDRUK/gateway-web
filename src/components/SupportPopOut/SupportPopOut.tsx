2f38a7ec3e4984e12e6b0d441ab44e60c91ab12c
philreekshdr <153518986+philreekshdr@users.noreply.github.com>
2025-11-13 15:38:20 +0000

    feat(GAT-7826): Update support centre links (#1376)

--- src/components/SupportPopOut/SupportPopOut.tsx
+++ src/components/SupportPopOut/SupportPopOut.tsx
@@ -7,7 +7,6 @@ import { usePathname } from "next/navigation";
 import Button from "@/components/Button";
 import Link from "@/components/Link";
 import {
-    CUSTOMER_PORTAL_RAISE_SUPPORT_URL,
     CUSTOMER_PORTAL_REPORT_BUG_URL,
     CUSTOMER_PORTAL_SUPPORT_URL,
 } from "@/config/hrefs";
@@ -54,11 +53,6 @@ const SupportPopOut = () => {
         {
             label: t("reportBug"),
             href: CUSTOMER_PORTAL_REPORT_BUG_URL,
-            isExternal: true,
-        },
-        {
-            label: t("requestSupport"),
-            href: CUSTOMER_PORTAL_RAISE_SUPPORT_URL,
             isExternal: true,
         },
     ];
