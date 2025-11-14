760225e2ef65ee6e3d59a0b2203445fb71869cce
Phil Reeks <phil.reeks@hdruk.ac.uk>
2025-11-14 14:41:29 +0000

    jest update

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
 import { server } from "./mocks/server";
 
 const nextRouterMock = require("next-router-mock");
