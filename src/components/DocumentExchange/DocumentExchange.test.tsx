fcaeed5f0654860012ee9287a043f6ac00ab8c0d
Phil Reeks <phil.reeks@hdruk.ac.uk>
2025-12-18 13:08:19 +0000

    Update tests

--- src/components/DocumentExchange/DocumentExchange.test.tsx
+++ src/components/DocumentExchange/DocumentExchange.test.tsx
@@ -1,42 +1,36 @@
 import { useForm, FormProvider } from "react-hook-form";
 import { fireEvent, screen, waitFor } from "@testing-library/react";
 import { render } from "@/utils/testUtils";
-import UploadFile, {
-    UploadFileProps,
-    UploadFormData,
-} from "./DocumentExchange";
+import { UploadFileProps, UploadFormData } from "./DocumentExchange";
+import DocumentExchange from "./DocumentExchange";
 
-const MockUploadFile = (props: Partial<UploadFileProps>) => {
+const MockDocumentExchange = (props: Partial<UploadFileProps>) => {
     const methods = useForm<UploadFormData>({
         defaultValues: {
             upload: {
-                value: [
-                    { id: 1, filename: "test-file-1.png" },
-                    { id: 2, filename: "test-file-2.png" },
-                ],
+                value: { id: 1, filename: "test-file-1.png" },
             },
         },
     });
 
     return (
         <FormProvider {...methods}>
-            <UploadFile {...props} control={methods.control} />
+            <DocumentExchange {...props} control={methods.control} />
         </FormProvider>
     );
 };
 
-describe("UploadFile Component", () => {
-    it("renders the UploadFile component and file select button", () => {
-        render(<MockUploadFile label="Upload Your File" />);
+describe("DocumentExchange Component", () => {
+    it("renders the DocumentExchange component and file select button", () => {
+        render(<MockDocumentExchange label="Upload Your File" />);
 
         expect(screen.getByText("Upload Your File")).toBeInTheDocument();
-        expect(screen.getByText("Upload")).toBeInTheDocument();
     });
 
     it("handles file removal", async () => {
         const onFileRemove = jest.fn();
         render(
-            <MockUploadFile
+            <MockDocumentExchange
                 onFileRemove={onFileRemove}
                 fileDownloadApiPath="/file/download"
                 label="Uploaded File"
@@ -45,48 +39,29 @@ describe("UploadFile Component", () => {
         );
 
         expect(screen.getByText("test-file-1.png")).toBeInTheDocument();
-        expect(screen.getByText("test-file-2.png")).toBeInTheDocument();
 
         const removeButton = screen.getByLabelText(
             "Remove file test-file-1.png"
         );
         fireEvent.click(removeButton);
 
-        await waitFor(() => expect(onFileRemove).toHaveBeenCalledWith(1));
-    });
-
-    it("handles multiple file uploads", async () => {
-        const onFileRemove = jest.fn();
-        render(
-            <MockUploadFile
-                onFileRemove={onFileRemove}
-                fileDownloadApiPath="/file/download"
-                label="Uploaded File"
-                allowMultipleFiles
-            />
-        );
-
-        expect(screen.getByText("test-file-1.png")).toBeInTheDocument();
-        expect(screen.getByText("test-file-2.png")).toBeInTheDocument();
-
-        const removeButton = screen.getByLabelText(
-            "Remove file test-file-2.png"
-        );
-        fireEvent.click(removeButton);
-
-        await waitFor(() => expect(onFileRemove).toHaveBeenCalledWith(2));
+        await waitFor(() => expect(onFileRemove).toHaveBeenCalled());
     });
 
     it("does not allow file upload if no file is selected", async () => {
         const onFileRemove = jest.fn();
         render(
-            <MockUploadFile
+            <MockDocumentExchange
                 onFileRemove={onFileRemove}
                 fileDownloadApiPath="/file/download"
                 label="Uploaded File"
                 allowMultipleFiles
+                fileSelectButtonText="Select file"
             />
         );
+
+        const reuploadButton = screen.getAllByText("Re-upload")[1];
+        fireEvent.click(reuploadButton);
 
         const uploadButton = screen.getByText("Upload");
         expect(uploadButton).toBeDisabled();
@@ -95,13 +70,16 @@ describe("UploadFile Component", () => {
     it("handles file upload button state correctly", async () => {
         const onFileRemove = jest.fn();
         render(
-            <MockUploadFile
+            <MockDocumentExchange
                 onFileRemove={onFileRemove}
                 fileDownloadApiPath="/file/download"
                 label="Uploaded File"
                 allowMultipleFiles
             />
         );
+
+        const reuploadButton = screen.getAllByText("Re-upload")[1];
+        fireEvent.click(reuploadButton);
 
         const uploadButton = screen.getByText("Upload");
         expect(uploadButton).toBeDisabled();
