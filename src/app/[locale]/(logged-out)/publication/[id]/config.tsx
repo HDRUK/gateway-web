12f166e23ce22e9ff20a16c643ddc7f1bd7f4205
Phil Reeks <phil.reeks@hdruk.ac.uk>
2026-01-16 10:55:43 +0000

    Add setup for publications page

---
+++ src/app/[locale]/(logged-out)/publication/[id]/config.tsx
@@ -0,0 +1,88 @@
+export enum FieldType {
+    TEXT = "text",
+    LIST_TEXT = "list",
+    LIST_LINK = "list-link",
+    DATE = "date",
+    TAG = "tag",
+    LINK = "link",
+}
+interface PublicationField {
+    path: string;
+    type: FieldType;
+    hideTooltip?: boolean;
+    hideLabel?: boolean;
+}
+
+export interface PublicationSection {
+    sectionName: string;
+    fields: PublicationField[];
+}
+
+const publicationFields: PublicationSection[] = [
+    {
+        sectionName: "abstract",
+        fields: [
+            {
+                path: "abstract",
+                type: FieldType.TEXT,
+                hideTooltip: true,
+                hideLabel: true,
+            },
+        ],
+    },
+    {
+        sectionName: "details",
+        fields: [
+            {
+                path: "journal_name",
+                type: FieldType.TEXT,
+            },
+            {
+                path: "authors",
+                type: FieldType.TEXT,
+            },
+            {
+                path: "team_id",
+                type: FieldType.TEXT,
+            },
+
+            {
+                path: "publication_type",
+                type: FieldType.TEXT,
+            },
+            {
+                path: "url",
+                type: FieldType.TEXT,
+            },
+            {
+                path: "paper_doi",
+                type: FieldType.LINK,
+            },
+            {
+                path: "year_of_publication",
+                type: FieldType.TEXT,
+            },
+        ],
+    },
+];
+
+const relatedContentAccordions = [
+    {
+        sectionName: "datasets",
+        fields: [],
+    },
+    {
+        sectionName: "dataUses",
+        fields: [],
+    },
+    {
+        sectionName: "tools",
+        fields: [],
+    },
+    {
+        sectionName: "collections",
+        fields: [],
+    },
+];
+
+export { publicationFields, relatedContentAccordions };
