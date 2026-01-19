bb63dc8a54ccfa877ff58d3f0c57fc7ef70771bd
Phil Reeks <phil.reeks@hdruk.ac.uk>
2026-01-19 13:57:46 +0000

    Add publication page

---
+++ src/app/[locale]/(logged-out)/publication/[id]/page.tsx
@@ -0,0 +1,103 @@
+import { get, isEmpty } from "lodash";
+import { getTranslations } from "next-intl/server";
+import { notFound } from "next/navigation";
+import Box from "@/components/Box";
+import CollectionsContent from "@/components/CollectionsContent";
+import DataUsesContent from "@/components/DataUsesContent";
+import DatasetsContent from "@/components/DatasetsContent";
+import LayoutDataItemPage from "@/components/LayoutDataItemPage";
+import ToolsContent from "@/components/ToolsContent";
+import Typography from "@/components/Typography";
+import ActiveListSidebar from "@/modules/ActiveListSidebar";
+import { DataStatus } from "@/consts/application";
+import { getPublication } from "@/utils/api";
+import metaData from "@/utils/metadata";
+import ActionBar from "./components/ActionBar";
+import PublicationContent from "./components/PublicationContent";
+import { publicationFields, relatedContentAccordions } from "./config";
+
+const TRANSLATION_PATH = "pages.publication";
+
+export const metadata = metaData({
+    title: "Publication",
+    description: "",
+});
+
+export default async function PublicationItemPage({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const t = await getTranslations(TRANSLATION_PATH);
+
+    const { id } = await params;
+    const data = await getPublication(id, {
+        suppressError: true,
+    });
+
+    if (!data || data?.status !== DataStatus.ACTIVE) notFound();
+
+    const populatedSections = publicationFields.filter(section =>
+        section.fields.some(field => !isEmpty(get(data, field.path)))
+    );
+
+    const activeLinkList = populatedSections
+        .concat(relatedContentAccordions)
+        .map(section => {
+            return {
+                label: t(section.sectionName),
+            };
+        });
+
+    return (
+        <LayoutDataItemPage
+            navigation={<ActiveListSidebar items={activeLinkList} />}
+            body={
+                <>
+                    <ActionBar />
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: 2,
+                            px: 3,
+                            py: 3,
+                        }}>
+                        <Typography
+                            variant="h2"
+                            sx={{ pt: 0.5, pb: 0.5, m: 0 }}
+                            id="anchor1">
+                            {data.paper_title}
+                        </Typography>
+
+                        <PublicationContent
+                            data={data}
+                            populatedSections={populatedSections}
+                        />
+
+                        <DatasetsContent
+                            datasets={data.datasets}
+                            anchorIndex={populatedSections.length + 1}
+                            translationPath={TRANSLATION_PATH}
+                        />
+                        <DataUsesContent
+                            datauses={data.durs}
+                            anchorIndex={populatedSections.length + 2}
+                            translationPath={TRANSLATION_PATH}
+                        />
+                        <ToolsContent
+                            tools={data.tools}
+                            anchorIndex={populatedSections.length + 4}
+                            translationPath={TRANSLATION_PATH}
+                        />
+                        <CollectionsContent
+                            collections={data.collections}
+                            anchorIndex={populatedSections.length + 4}
+                            translationPath={TRANSLATION_PATH}
+                        />
+                    </Box>
+                </>
+            }
+        />
+    );
+}
