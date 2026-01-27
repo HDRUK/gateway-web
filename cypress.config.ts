8f9ed823515f0a649b40baf82d55d4f303873fa4
Phil Reeks <phil.reeks@hdruk.ac.uk>
2026-01-27 10:55:26 +0000

    Add base cypress and workflow

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "cypress";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+export default defineConfig({
+    e2e: {
+        baseUrl: "http://localhost:3000",
+        env: {
+            API_URL: process.env.NEXT_PUBLIC_API_V1_URL,
+        },
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+            return config;
+        },
+    },
+});
