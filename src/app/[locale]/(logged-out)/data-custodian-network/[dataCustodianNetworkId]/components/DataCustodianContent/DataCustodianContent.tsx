7e037404e0a98f91ef2a5c0a5aa86b70675dc800
Phil Reeks <phil.reeks@hdruk.ac.uk>
2026-07-08 13:24:57 +0100

    Updates to data custodian network associated content

--- src/app/[locale]/(logged-out)/data-custodian-network/[dataCustodianNetworkId]/components/DataCustodianContent/DataCustodianContent.tsx
+++ src/app/[locale]/(logged-out)/data-custodian-network/[dataCustodianNetworkId]/components/DataCustodianContent/DataCustodianContent.tsx
@@ -4,7 +4,7 @@ import { Fragment } from "react";
 import { Link } from "@mui/material";
 import { useTranslations } from "next-intl";
 import { DataProvider as DataCustodians } from "@/interfaces/DataProvider";
-import AccordionSection from "@/components/AccordionSection";
+import AccordionSectionSplit from "@/components/AccordionSectionSplit";
 import { RouteName } from "@/consts/routeName";
 
 const TRANSLATION_PATH =
@@ -21,58 +21,61 @@ export default function DataCustodianContent({
 }: DataCustodianContentProps) {
     const t = useTranslations(TRANSLATION_PATH);
 
+    const renderCard = ({
+        name,
+        id,
+        datasets_count,
+        publications_count,
+        tools_count,
+        collections_count,
+        durs_count,
+    }: DataCustodians) => (
+        <Fragment key={`data-custodian-${id}`}>
+            <Link
+                href={`/${RouteName.DATA_CUSTODIANS_ITEM}/${id}`}
+                style={{ textDecoration: "none" }}>
+                {name}
+            </Link>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>{`${t("datasets", {
+                            length: datasets_count,
+                        })}`}</td>
+                        <td>{`${t("datause", {
+                            length: durs_count,
+                        })}`}</td>
+                    </tr>
+                    <tr>
+                        <td>{`${t("tools", {
+                            length: tools_count,
+                        })}`}</td>
+                        <td>{`${t("publications", {
+                            length: publications_count,
+                        })}`}</td>
+                    </tr>
+                    <tr>
+                        <td>{`${t("collections", {
+                            length: collections_count,
+                        })}`}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Fragment>
+    );
+
     return (
-        <AccordionSection
+        <AccordionSectionSplit
             id={`anchor${anchorIndex}`}
             disabled={!dataCustodians.length}
-            heading={t("heading", {
-                length: dataCustodians.length,
-            })}
+            heading={t("title")}
+            ownedHeading={t("title")}
+            ownedCount={dataCustodians.length}
+            associatedHeading=""
+            associatedCount={0}
             defaultExpanded={dataCustodians.length > 0}
-            contents={dataCustodians.map(
-                ({
-                    name,
-                    id,
-                    datasets_count,
-                    publications_count,
-                    tools_count,
-                    collections_count,
-                    durs_count,
-                }) => (
-                    <Fragment key={`data-custodian-${id}`}>
-                        <Link
-                            href={`/${RouteName.DATA_CUSTODIANS_ITEM}/${id}`}
-                            style={{ textDecoration: "none" }}>
-                            {name}
-                        </Link>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>{`${t("datasets", {
-                                        length: datasets_count,
-                                    })}`}</td>
-                                    <td>{`${t("datause", {
-                                        length: durs_count,
-                                    })}`}</td>
-                                </tr>
-                                <tr>
-                                    <td>{`${t("tools", {
-                                        length: tools_count,
-                                    })}`}</td>
-                                    <td>{`${t("publications", {
-                                        length: publications_count,
-                                    })}`}</td>
-                                </tr>
-                                <tr>
-                                    <td>{`${t("collections", {
-                                        length: collections_count,
-                                    })}`}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </Fragment>
-                )
-            )}
+            ownedContents={dataCustodians.map(renderCard)}
+            associatedContents={[]}
         />
     );
 }
